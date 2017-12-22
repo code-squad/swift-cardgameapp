ffae46dd508ff28c2cc473ab0e14c96c1c2cca72
yang hee jung <puuurm@naver.com>
2017-12-22 14:07:32 +0900

    Create card images in the view

--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -9,21 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    lazy var cardImageViews: [UIImageView] = { [unowned self] in
+        var imageViews = [UIImageView]()
+        let image = UIImage(named: "card-back")
+        for i in 0...6 {
+            imageViews.append(UIImageView(image: image!))
+            imageViews[i].translatesAutoresizingMaskIntoConstraints = false
+            imageViews[i].heightAnchor.constraint(equalTo: imageViews[i].widthAnchor, multiplier: 1.27).isActive = true
+        }
+        return imageViews
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        makeBackGroundImage()
+        allocateCardImageViews()
+    }
+
+    private func makeBackGroundImage() {
         guard let patternImage = UIImage(named: "bg_pattern") else {
             return
         }
         view.backgroundColor = UIColor.init(patternImage: patternImage)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func allocateCardImageViews() {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        for i in 0...6 {
+            stackView.addArrangedSubview(cardImageViews[i])
+        }
+        stackView.distribution = .fillEqually
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
     }
-
 
 }
 
