b1ded97bd956111e1c339f1eb9f1e5fdd6ce266f
yang hee jung <puuurm@naver.com>
2017-12-23 18:51:57 +0900

    Add a Card class unit test for TDD

---
+++ CardGameApp/CardGameAppTests/CardClassTest.swift
@@ -0,0 +1,29 @@
+//
+//  CardClassTest.swift
+//  CardGameAppTests
+//
+//  Created by yangpc on 2017. 12. 22..
+//  Copyright © 2017년 yang hee jung. All rights reserved.
+//
+
+import XCTest
+@testable import CardGameApp
+
+class CardClassTest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
