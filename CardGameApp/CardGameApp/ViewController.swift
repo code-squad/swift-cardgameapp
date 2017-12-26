0e43f1d85709862f3aa390e66e59baa0738eaf31
yang hee jung <puuurm@naver.com>
2017-12-27 03:09:01 +0900

    Place views for poker games

--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -9,21 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+
+    // MARK: Properties...
+    var cardDeck = CardDeck()
+    lazy var emptyViews: [UIView] = { [unowned self] in
+        var views = [UIView]()
+        for _ in 0...3 {
+            let emptyView = UIView()
+            emptyView.layer.borderWidth = 1
+            emptyView.layer.borderColor = UIColor.white.cgColor
+            views.append(emptyView)
+        }
+        return views
+    }()
+
+    lazy var backCardImageView: UIImageView = { [unowned self] in
+        let image = makeRandomCardImages(1).first
+        return UIImageView(image: image!)
+    }()
+
     lazy var cardImageViews: [UIImageView] = { [unowned self] in
         var imageViews = [UIImageView]()
-        let image = UIImage(named: "card-back")
-        for i in 0...6 {
-            imageViews.append(UIImageView(image: image!))
-            makeCardViewPosition(view: &imageViews[i])
-        }
+        let images = makeRandomCardImages(7)
+        images.forEach {imageViews.append(UIImageView(image: $0))}
         return imageViews
     }()
+
+    // MARK: Override...
 
     override func viewDidLoad() {
         super.viewDidLoad()
         makeBackGroundImage()
-        allocateCardImageViews()
+        setUIViewLayout()
     }
+
+    // MARK: Methods...
 
     private func makeBackGroundImage() {
         guard let patternImage = UIImage(named: "bg_pattern") else {
@@ -32,28 +52,88 @@ class ViewController: UIViewController {
         view.backgroundColor = UIColor.init(patternImage: patternImage)
     }
 
-    private func allocateCardImageViews() {
-        var stackView = UIStackView()
-        stackView.axis = .horizontal
-        for i in 0...6 {
-            stackView.addArrangedSubview(cardImageViews[i])
+    func pickCards(number: Int) -> [Card]? {
+        cardDeck.shuffle()
+        let cards = cardDeck.pickCards(number: number)
+        return cards
+    }
+
+    func makeRandomCardImages(_ number: Int) -> [UIImage] {
+        guard let cards = pickCards(number: number) else {
+            return []
         }
-        stackView.distribution = .fillEqually
-        stackView.spacing = 2
-        view.addSubview(stackView)
-        makeStackViewPosition(view: &stackView)
+        if number == 1 { return [(cards.first?.makeBackImage())!] }
+        var images = [UIImage]()
+        cards.forEach { images.append($0.makeImage()!) }
+        return images
     }
 
-    private func makeStackViewPosition(view: inout UIStackView) {
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
-        view.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
-        view.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+    func setUIViewLayout() {
+        emptyViews.forEach { $0.setLatio() }
+        backCardImageView.setLatio()
+        cardImageViews.forEach { $0.setLatio() }
+
+        let frontStackVeiw = UIStackView()
+        frontStackVeiw.axis = .horizontal
+        emptyViews.forEach { frontStackVeiw.addArrangedSubview($0) }
+        frontStackVeiw.distribution = .fillEqually
+        frontStackVeiw.spacing = 2
+        view.addSubview(frontStackVeiw)
+        frontStackVeiw.setAutolayout()
+        frontStackVeiw.top(equal: view.safeAreaLayoutGuide.topAnchor)
+        frontStackVeiw.leading(equal: view.leadingAnchor)
+
+        view.addSubview(backCardImageView)
+        backCardImageView.setAutolayout()
+        backCardImageView.top(equal: view.safeAreaLayoutGuide.topAnchor)
+        backCardImageView.trailing(equal: view.trailingAnchor)
+
+        let backStackVeiw = UIStackView()
+        backStackVeiw.axis = .horizontal
+        cardImageViews.forEach { backStackVeiw.addArrangedSubview($0) }
+        backStackVeiw.distribution = .fillEqually
+        backStackVeiw.spacing = 2
+        view.addSubview(backStackVeiw)
+        backStackVeiw.setAutolayout()
+        backStackVeiw.top(equal: frontStackVeiw.bottomAnchor, constant: 2)
+        backStackVeiw.leading(equal: view.leadingAnchor)
+        backStackVeiw.trailing(equal: view.trailingAnchor)
+
+        frontStackVeiw.trailing(equal: cardImageViews[3].trailingAnchor)
+        backCardImageView.leading(equal: cardImageViews[6].leadingAnchor)
     }
 
-    private func makeCardViewPosition(view: inout UIImageView) {
-        view.translatesAutoresizingMaskIntoConstraints = false
-        view.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1.27).isActive = true
+}
+
+protocol CardViewLayoutProtocol {
+    func setLatio()
+}
+
+extension UIView: CardViewLayoutProtocol {
+    func setLatio() {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.27).isActive = true
+    }
+}
+
+extension UIView {
+    func setAutolayout() {
+        self.translatesAutoresizingMaskIntoConstraints = false
     }
 
+    func top(equal: NSLayoutYAxisAnchor, constant: CGFloat = 0) {
+        self.topAnchor.constraint(equalTo: equal, constant: constant).isActive = true
+    }
+
+    func bottom(equal: NSLayoutYAxisAnchor, constant: CGFloat = 0) {
+        self.bottomAnchor.constraint(equalTo: equal, constant: constant).isActive = true
+    }
+
+    func leading(equal: NSLayoutXAxisAnchor, constant: CGFloat = 0) {
+        self.leadingAnchor.constraint(equalTo: equal, constant: constant).isActive = true
+    }
+
+    func trailing(equal: NSLayoutXAxisAnchor, constant: CGFloat = 0) {
+        self.trailingAnchor.constraint(equalTo: equal, constant: constant).isActive = true
+    }
 }
