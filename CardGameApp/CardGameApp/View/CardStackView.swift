4651dc3645805800537b2fbe94456b899df08bcd
yang hee jung <puuurm@naver.com>
2018-01-03 18:16:18 +0900

    Add comments to the CardStackView class

--- CardGameApp/CardGameApp/View/CardStackView.swift
+++ CardGameApp/CardGameApp/View/CardStackView.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class CardStackView: UIView {
+    // 카드 이미지 스택
     var cardStackImageViews: [UIImageView] = [UIImageView]()
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -19,7 +20,9 @@ class CardStackView: UIView {
     }
 }
 
+// MARK: Make ImageView
 extension CardStackView {
+    // 카드 스택 이미지 뷰를 처음 생성한다.
     func makeCardStackImageView(_ cardStack: CardStack?) {
         var imageViews = [UIImageView]()
         let images = makeCardImages(cardStack)
@@ -27,11 +30,13 @@ extension CardStackView {
         cardStackImageViews = imageViews
     }
 
+    // 카드 스택 이미지 뷰를 변경한다.
     func changeImages(_ cardStack: CardStack?) {
         var images = makeCardImages(cardStack)
         cardStackImageViews.forEach { $0.image = images.removeFirst() }
     }
 
+    // 카드 이미지 뷰를 만드는 함수 (마지막 카드만 카드 앞면.)
     private func makeCardImages(_ cardStack: CardStack?) -> [UIImage] {
         var images = [UIImage]()
         guard var stack = cardStack else {
@@ -51,11 +56,16 @@ extension CardStackView {
 
     }
 
+}
+
+// MARK: Layout
+extension CardStackView {
     func setLayout() {
         setEmptyStackViewLayout()
         setCardStackViewLayout()
     }
 
+    // 이미지가 있는 카드 스택 뷰 배치
     private func setCardStackViewLayout() {
         cardStackImageViews.forEach { (imageview: UIImageView) in
             let i = cardStackImageViews.index(of: imageview) ?? cardStackImageViews.endIndex
@@ -68,6 +78,7 @@ extension CardStackView {
         }
     }
 
+    // 빈 카드 카드 스택 뷰 배치
     private func setEmptyStackViewLayout() {
         let emptyView = UIView().makeEmptyView()
         self.addSubview(emptyView)
