e4aa9edf59b6d2ee17542a1dc121d52341c46321
DongJin Lee <comets195@gmail.com>
2018-01-26 19:16:15 +0900

    Fix Card Margin Ratio..

--- CardGameApp/CardGameApp/CardView.swift
+++ CardGameApp/CardGameApp/CardView.swift
@@ -10,11 +10,13 @@ import UIKit
 
 class CardView: UIView {
     func makeCardView(screenWidth: CGFloat, index: Int) -> UIImageView {
-        let cardWidth = (screenWidth / 7) - (screenWidth / 65)
-        let margin = (screenWidth - (cardWidth * 7)) / 7
+        let marginRatio: CGFloat = 70
+        let cardsNumber: CGFloat = 7
+        let cardWidth = (screenWidth / cardsNumber) - (screenWidth / marginRatio)
+        let margin = (screenWidth - (cardWidth * cardsNumber)) / (cardsNumber + 1)
         let card = UIImageView(image: UIImage(named: "card_back"))
-        let xCoordinate = ((cardWidth + cardWidth / 10) * CGFloat(index)) + margin
-        card.frame = CGRect(x: xCoordinate, y: 32, width: cardWidth, height: (screenWidth / 7) * 1.27)
+        let xCoordinate = ((cardWidth + margin) * CGFloat(index)) + margin
+        card.frame = CGRect(x: xCoordinate, y: 32, width: cardWidth, height: (screenWidth / cardsNumber) * 1.27)
         return card
     }
 }
