e52b5205bff7911227391901a861003a6490d859
ChocOZerO <nayalth@gmail.com>
2018-01-26 14:22:54 +0900

    [Init]CardGame #1

---
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CardGameApp
+//
+//  Created by TaeHyeonLee on 2018. 1. 26..
+//  Copyright © 2018년 ChocOZerO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg_pattern.png")!)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
