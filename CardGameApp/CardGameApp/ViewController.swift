51e10fb5ef8ad62865da6ed15c00a9e669c4358b
undervine <undervine@naver.com>
2018-01-30 15:38:08 +0900

    [초기설정] 프로젝트 세팅

---
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardGameApp
+//
+//  Created by 심 승민 on 2018. 1. 30..
+//  Copyright © 2018년 심 승민. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
