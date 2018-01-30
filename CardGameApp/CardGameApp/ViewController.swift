6bb22e4106e9bafe0f1e89019a2ce163fc119040
undervine <undervine@naver.com>
2018-01-30 18:22:03 +0900

    [추가] 카드 7장 균등하게 출력

--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -12,8 +12,40 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor(patternImage: .init(imageLiteralResourceName: "bg_pattern"))
+        print(UIApplication.shared.statusBarFrame.height)
+        viewRespectsSystemMinimumLayoutMargins = false
+        view.layoutMargins = UIEdgeInsets(top: UIApplication.shared.statusBarFrame.height,
+                                          left: 10,
+                                          bottom: 10,
+                                          right: 10)
+        var cardPosition = CGPoint(x: view.layoutMargins.left, y: view.layoutMargins.top)
+        while cardPosition.x+cardSize.width <= view.frame.maxX {
+            let cardView = generateCard(cardPosition)
+            view.addSubview(cardView)
+            cardPosition = CGPoint(x: cardView.frame.origin.x+cardView.frame.width+cardMargins,
+                                   y: view.layoutMargins.top)
+        }
+    }
+
+    var numberOfCards: CGFloat = 7
+
+    var cardMargins: CGFloat {
+        return (view.frame.size.width-cardSize.width*numberOfCards)/numberOfCards
+    }
+
+    var cardSize: CGSize {
+        let width = view.frame.size.width/numberOfCards-15
+        let height = width*1.27
+        return CGSize(width: width, height: height)
+    }
+
+    private func generateCard(_ origin: CGPoint) -> UIImageView {
+        let cardImage = UIImage(imageLiteralResourceName: "card-back")
+        let cardImageView = UIImageView(image: cardImage)
+        cardImageView.frame.origin = origin
+        cardImageView.frame.size = cardSize
+        return cardImageView
     }
 
 }
-
