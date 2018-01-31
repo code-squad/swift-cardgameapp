6485660b8b05e6feea305465874ef3243018825d
DongJin Lee <comets195@gmail.com>
2018-01-31 16:29:11 +0900

    [Add] Table객체 추가 및 VC 화면 뿌리기. #7

--- CardGameApp/CardGameApp/UIImageView+CardView.swift
+++ CardGameApp/CardGameApp/UIImageView+CardView.swift
@@ -9,17 +9,46 @@
 import UIKit
 
 extension UIImageView {
-    func makeCardView(index: Int, yCoordinate: CGFloat) {
-        let screenWidth = UIScreen.main.fixedCoordinateSpace.bounds.width
-        let marginRatio: CGFloat = 70
-        let cardsNumber: CGFloat = 7
-        let cardWidth = (screenWidth / cardsNumber) - (screenWidth / marginRatio)
-        let margin = (screenWidth - (cardWidth * cardsNumber)) / (cardsNumber + 1)
-        let xCoordinate = ((cardWidth + margin) * CGFloat(index)) + margin        
-        self.frame = CGRect(x: xCoordinate, y: yCoordinate, width: cardWidth, height: (screenWidth / cardsNumber) * 1.27)
+    func makeBasicView() {
+        self.bounds = CGRect(origin: CGPoint.zero, size: self.cardSize())
         self.layer.borderWidth = 1
         self.layer.cornerRadius = 5
         self.layer.masksToBounds = true
         self.layer.borderColor = UIColor.white.cgColor
+    }
+    
+    func makeCardView(index: CGFloat, yPoint: CGFloat) {
+        let xPoint = ((self.cardSize().width + self.marginBetweenCard()) * CGFloat(index)) + self.marginBetweenCard()
+        self.makeBasicView()
+        self.frame.origin = CGPoint(x: xPoint, y: yPoint)
+    }
+    
+    func cardSize() -> CGSize {
+        let screenWidth = UIScreen.main.fixedCoordinateSpace.bounds.width
+        let marginRatio: CGFloat = 70
+        let cardColumns: CGFloat = 7
+        let cardWidth = (screenWidth / cardColumns) - (screenWidth / marginRatio)
+        let cardHeight = (screenWidth / cardColumns) * 1.27
+        return CGSize(width: cardWidth, height: cardHeight)
+    }
+    
+    func marginBetweenCard() -> CGFloat {
+        let cardFrame = self.cardSize()
+        let screenWidth = UIScreen.main.fixedCoordinateSpace.bounds.width
+        let margin = (screenWidth - (cardFrame.width * 7)) / 8
+        return margin
+    }
+    
+    func makeRefreshButton() {
+        guard let image = UIImage(named: "cardgameapp-refresh-app") else { return }
+        let screenWidth = UIScreen.main.fixedCoordinateSpace.bounds.width
+        let yPoint = UIApplication.shared.statusBarFrame.height
+        let ratio = screenWidth / 1000
+        let imageWidth = ratio * image.size.width
+        self.frame = CGRect(x: screenWidth - (imageWidth + (imageWidth / CGFloat(3))),
+                            y: yPoint + 20,
+                            width: ratio * image.size.width,
+                            height: ratio * image.size.height)
+        self.contentMode = UIViewContentMode.scaleAspectFit
     }
 }
