ff4f1d28a59ac2dca847d45c75e9b3de18caddf0
undervine <undervine@gmail.com>
2018-02-06 01:10:11 +0900

    [추가] 스택뷰 설정 시 공통적으로 필요한 정보만 담는 구조체 추가

---
+++ CardGameApp/CardGameApp/View/StackViewInfo.swift
@@ -0,0 +1,16 @@
+//
+//  StackViewInfo.swift
+//  CardGameApp
+//
+//  Created by 심 승민 on 2018. 2. 5..
+//  Copyright © 2018년 심 승민. All rights reserved.
+//
+
+import UIKit
+
+struct StackViewInfo {
+    private(set) var axis: UILayoutConstraintAxis
+    private(set) var distribution: UIStackViewDistribution
+    private(set) var spacing: CGFloat
+    private(set) var bottomMargin: CGFloat
+}
