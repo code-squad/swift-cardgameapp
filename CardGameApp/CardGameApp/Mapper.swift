10b5417406c1f1dcd37353439fadf569dcaa891d
undervineg <undervineg@gmail.com>
2018-02-07 18:39:22 +0900

    [수정] static 메소드로 변경
    서브 메소드들은 private으로 변경

--- CardGameApp/CardGameApp/Mapper.swift
+++ CardGameApp/CardGameApp/Mapper.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+class Mapper {
+    static func mapFrontImageName(of card: Card) -> String? {
+        return Mapper.shapeName(of: card.info.shape) + Mapper.numberName(of: card.info.number)
+    }
+
+    private static func numberName(of number: Number) -> String {
+        return String(number.rawValue)
+    }
+
+    private static func shapeName(of shape: Shape) -> String {
+        var shapeName = ""
+        switch shape {
+        case .clubs: shapeName = "c"
+        case .diamonds: shapeName = "d"
+        case .hearts: shapeName = "h"
+        case .spades: shapeName = "s"
+        }
+        return shapeName
+    }
+}
