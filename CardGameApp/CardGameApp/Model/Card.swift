9edb8552c68103ff9b7cd1f95572ce88b28eb5c4
undervineg <undervineg@gmail.com>
2018-02-07 18:12:01 +0900

    [제거] description 제거. cardInfo()로 대체 가능

--- CardGameApp/CardGameApp/Model/Card.swift
+++ CardGameApp/CardGameApp/Model/Card.swift
@@ -24,12 +24,12 @@ class Card {
 }
 
 // 카드 한 장 출력 포맷. 모양+숫자 형태.
-extension Card: CustomStringConvertible {
-    var description: String {
-        return self.shape.description + self.number.description
-    }
-
-}
+//extension Card: CustomStringConvertible {
+//    var description: String {
+//        return self.shape.description + self.number.description
+//    }
+//
+//}
 
 // 카드를 비교할 경우, 카드의 숫자를 비교. (Shape과 Number의 캡슐화를 위함.)
 extension Card: Equatable, Comparable {
