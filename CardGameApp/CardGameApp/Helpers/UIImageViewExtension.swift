d7ea7e8549762fb5bad0c560a859e47c6716baea
undervineg <undervineg@gmail.com>
2018-02-11 01:31:12 +0900

    [추가] 이미지뷰 사이즈, 테두리 스타일 설정 함수들 추가

--- CardGameApp/CardGameApp/Helpers/UIImageViewExtension.swift
+++ CardGameApp/CardGameApp/Helpers/UIImageViewExtension.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2018년 심 승민. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIImageView {
+    // 카드크기 일정하게 제약
+    func setCardSizeTo(_ size: CGSize) {
+        self.widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        self.heightAnchor.constraint(equalToConstant: size.height).isActive = true
+    }
+
+    // 카드 테두리 설정
+    func setDefaultCardBorderStyle() {
+        self.layer.cornerRadius = DeckViewModel.CardPresentable.cornerRadius
+        self.clipsToBounds = DeckViewModel.CardPresentable.clipToBounds
+        self.layer.borderColor = DeckViewModel.CardPresentable.borderColor
+        self.layer.borderWidth = DeckViewModel.CardPresentable.borderWidth
+    }
+}
