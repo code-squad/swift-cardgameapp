aa547f337b530dc2237ddfdb7ee6896bd93bffa3
undervineg <undervineg@gmail.com>
2018-02-11 02:08:41 +0900

    [수정] 뷰 생성과 데이터 삽입 시점을 분리
    기존의 Custom View 타입들은 제거하고, UIImageView와 UIStack을 사용하여 각 영역별 표시할 뷰 컴포넌트 생성.
    뷰 생성 시에는 뷰만 생성하며, 뷰모델이 세팅되고 나면 뷰와 데이터를 바인딩 하여 데이터가 변경될 때 뷰도 변경되도록 수정.
    + 뷰 위치값, 상수값은 내부 구조체로 분리

--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -8,96 +8,149 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    private var deck: Deck?
-    private var dealedPosition: CGPoint?
-    private var vacantPosition: CGPoint?
-    private var sparePosition: CGPoint?
-    private var revealedPosition: CGPoint?
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
+    private var position: Position!
+    private var deckViewModel: DeckViewModel! {
+        didSet {
+            bindDataWithSpareCard()
+            bindDataWithRevealedCard()
+            bindDataWithArrangedCards()
+            bindDataWithFetchedCards()
+        }
+    }
 
-    private lazy var revealedCardViewStack: CardViewStack = {
-        let size = CGSize(width: cardSize.width-Constants.GameBoard.horizontalStackSpacing, height: cardSize.height)
-        return CardViewStack(frame: CGRect(origin: revealedPosition!, size: size), cardViews: [])
+    // 우측 상단에 위치. fetch 후 남은 카드 놓임.
+    private lazy var spareCardView: UIImageView = { [unowned self] in
+        let size = CGSize(width: cardSize.width-Constants.horizontalStackSpacing, height: cardSize.height)
+        let imageView = UIImageView(frame: CGRect(origin: position.spareCardsPosition, size: size))
+        imageView.setCardSizeTo(cardSize)
+        imageView.setDefaultCardBorderStyle()
+        if let topCardViewModelOnSpareDummy = deckViewModel.spareCardDummy.topCardViewModel(false) {
+            // 뷰에 데이터 설정
+            imageView.image = topCardViewModelOnSpareDummy.image
+        }
+        // 제스처인식기 설치
+        let singleTapGestureRecognizer = UITapGestureRecognizer(target: self,
+                                                                action: #selector(handleSingleTapOnSpare(_:)))
+        singleTapGestureRecognizer.numberOfTapsRequired = 1
+        imageView.addGestureRecognizer(singleTapGestureRecognizer)
+        imageView.isUserInteractionEnabled = true
+        return imageView
     }()
 
-    private lazy var spareCardViewStack: SpareCardViewStack = {
-        let spareCardViews = makeCardViews(count: deck?.remnants().count ?? 0)
-        let size = CGSize(width: cardSize.width-Constants.GameBoard.horizontalStackSpacing, height: cardSize.height)
-        let spareCardViewStack = SpareCardViewStack(frame: CGRect(origin: sparePosition!, size: size),
-                                                    cardViews: spareCardViews,
-                                                    revealedStackView: revealedCardViewStack)
-        return spareCardViewStack
+    @objc func handleSingleTapOnSpare(_ sender: UITapGestureRecognizer) {
+        deckViewModel.updateSpareTopCard()
+    }
+
+    // spare 카드뷰 왼쪽편에 위치. spare 카드뷰에서 뒤집은 카드 놓임.
+    private lazy var revealedCardView: UIImageView = { [unowned self] in
+        let size = CGSize(width: cardSize.width-Constants.horizontalStackSpacing, height: cardSize.height)
+        let imageView = UIImageView(frame: CGRect(origin: position.revealedCardsPosition, size: size))
+        imageView.setCardSizeTo(cardSize)
+        imageView.setDefaultCardBorderStyle()
+        return imageView
     }()
 
-    private lazy var vacantCardViewStack: VacantCardViewStack = {
-        return VacantCardViewStack(
-            frame: CGRect(origin: vacantPosition!,
-                          size: CGSize(width: view.frame.width-cardSize.width*3, height: cardSize.height)),
-            sizeOf: cardSize,
-            spaceCount: Constants.GameBoard.vacantSpaceCount,
-            spacing: Constants.GameBoard.horizontalStackSpacing)
+    // 좌측 상단에 위치될 카드뷰 4개 (처음엔 비어있음). 카드 순서에 맞게 정리되어 놓일 카드뷰.
+    private lazy var arrangedCardViews: UIStackView = { [unowned self] in
+        let size = CGSize(width: view.frame.width-cardSize.width*3, height: cardSize.height)
+        let stackView = UIStackView(frame: CGRect(origin: position.arrangedCardsPosition, size: size))
+        for _ in 0..<DeckViewModel.InitialGameSettings.spareViewCount {
+            let imageView = UIImageView()
+            imageView.frame.size = cardSize
+            imageView.setCardSizeTo(cardSize)
+            imageView.setDefaultCardBorderStyle()
+            stackView.addArrangedSubview(imageView)
+        }
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.spacing = Constants.horizontalStackSpacing
+        return stackView
     }()
 
-    private lazy var dealedCardViewStackContainer: DealedCardViewStackContainer = {
-        let verticalCardViewStacks = dealedCardViewStacks(of: Constants.GameBoard.maxStud)
-        let frame = CGRect(origin: dealedPosition!,
-                           size: CGSize(width: view.frame.width,
-                                        height: view.frame.height-dealedPosition!.y))
-        return DealedCardViewStackContainer(frame: frame,
-                                            dealedCardViewStacks: verticalCardViewStacks,
-                                            spacing: Constants.GameBoard.horizontalStackSpacing)
+    // 하단에 놓일 스택뷰 7개. 각 스택뷰 내부에는 fetch된 카드들이 놓임.
+    private lazy var fetchedStackViews: [UIStackView] = { [unowned self] in
+        var stacks: [UIStackView] = []
+        for stud in 1...DeckViewModel.InitialGameSettings.maxStud {
+            let stack = UIStackView(frame: .zero)
+            let currentFetchedCardDummy = deckViewModel.fetchedCardDummys[stud-1]
+            let imageViews = currentFetchedCardDummy.existCardViewModels(true).map { viewModel -> UIImageView in
+                let imageView = UIImageView(image: viewModel.image)
+                imageView.setCardSizeTo(cardSize)
+                imageView.setDefaultCardBorderStyle()
+                return imageView
+            }
+            stack.addArrangedSubviews(view: imageViews)
+            stack.axis = .vertical
+            stack.distribution = .fill
+            stack.spacing = -cardSize.height*0.7
+            let bottomMarginToLastCard =
+                (view.frame.height-position.fetchedCardsPosition.y)-(CGFloat(stud-1)*cardSize.height*0.3+cardSize.height)
+            stack.setBottomMargin(bottomMarginToLastCard)
+            stacks.append(stack)
+        }
+        return stacks
     }()
 
-    private lazy var cardSize: CGSize = {
-        guard let view = view else { return CGSize() }
-        let width = view.frame.size.width/7
-        let height = width*1.27
-        return CGSize(width: width, height: height)
+    // 하단 7개 스택뷰들을 가로로 정리하는 컨테이너 스택.
+    private lazy var fetchedStackViewContainer: UIStackView = { [unowned self] in
+        let size = CGSize(width: view.frame.width, height: view.frame.height-position.fetchedCardsPosition.y)
+        let stackContainer = UIStackView(frame: CGRect(origin: position.fetchedCardsPosition, size: size))
+        self.fetchedStackViews.forEach {
+            stackContainer.addArrangedSubview($0)
+        }
+        stackContainer.axis = .horizontal
+        stackContainer.distribution = .fillEqually
+        stackContainer.spacing = Constants.horizontalStackSpacing
+        return stackContainer
+    }()
+
+    private lazy var vacantCardStackView: UIStackView = { [unowned self] in
+        let size = CGSize(width: view.frame.width-cardSize.width*3, height: cardSize.height)
+        let vacantCardStackView = UIStackView(frame: CGRect(origin: position.arrangedCardsPosition, size: size))
+        for _ in 0..<DeckViewModel.InitialGameSettings.vacantSpaceCount {
+            let imageView = UIImageView()
+            imageView.setCardSizeTo(cardSize)
+            imageView.setDefaultCardBorderStyle()
+            vacantCardStackView.addArrangedSubview(imageView)
+        }
+        vacantCardStackView.axis = .horizontal
+        vacantCardStackView.distribution = .fillEqually
+        vacantCardStackView.spacing = Constants.horizontalStackSpacing
+        return vacantCardStackView
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // 배경 패턴 그림
         drawBackground()
-        // 위치 지정
-        vacantPosition = CGPoint(x: view.layoutMargins.left, y: view.layoutMargins.top)
-        dealedPosition = CGPoint(x: view.layoutMargins.left, y: view.layoutMargins.top+cardSize.height+15)
-        let spareX = view.frame.width -
-                        (view.layoutMargins.right+cardSize.width)+Constants.GameBoard.horizontalStackSpacing
-        sparePosition = CGPoint(x: spareX, y: view.layoutMargins.top)
-        revealedPosition = CGPoint(x: sparePosition!.x-cardSize.width-Constants.GameBoard.horizontalStackSpacing,
-                                        y: view.layoutMargins.top)
-        initGameBoard()
-        // 배치
-        view.addSubview(dealedCardViewStackContainer)
-        view.addSubview(vacantCardViewStack)
-        view.addSubview(spareCardViewStack)
-        view.addSubview(revealedCardViewStack)
+        // 위치값
+        position = Position(on: self.view, with: cardSize)
+        // 뷰모델 생성 (-> 뷰모델 초기설정 -> 바인딩)
+        deckViewModel = DeckViewModel()
+
+        // 뷰 배치
+        view.addSubview(spareCardView)
+        view.addSubview(revealedCardView)
+        view.addSubview(arrangedCardViews)
+        view.addSubview(fetchedStackViewContainer)
     }
 
-    private func initGameBoard() {
-        // 덱 생성 및 셔플
-        resetDeck()
-        dealedCardViewStackContainer.arrangedSubviews.forEach { (subview) in
-            (subview as? DealedCardViewStack)?.dataSource = self
-        }
-        spareCardViewStack.dataSource = self
-    }
-
-    private func resetDeck() {
-        deck = Deck()
-        deck?.shuffle()
-    }
+    private lazy var cardSize: CGSize = { [unowned self] in
+        guard let view = view else { return CGSize() }
+        let width = view.frame.size.width/7
+        let height = width*1.27
+        return CGSize(width: width, height: height)
+    }()
 
     override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
         if motion == .motionShake {
-            initGameBoard()
-            spareCardViewStack.reset()
+            deckViewModel.reset()
         }
     }
 
     private func drawBackground() {
-        view.backgroundColor = UIColor(patternImage: .init(imageLiteralResourceName: Constants.ImageName.background))
+        view.backgroundColor = UIColor(patternImage: .init(imageLiteralResourceName: Constants.background))
         viewRespectsSystemMinimumLayoutMargins = false
         view.layoutMargins = UIEdgeInsets(top: UIApplication.shared.statusBarFrame.height,
                                           left: 0,
@@ -107,45 +160,81 @@ class ViewController: UIViewController {
 
 }
 
-extension ViewController: CardViewStackDataSource {
-    func spareCards() -> CardStack? {
-        let remnants = deck?.remnants()
-        return remnants
+extension ViewController {
+    struct Constants {
+        static let background: String = "bg_pattern"
+        static let horizontalStackSpacing: CGFloat = 4
     }
 
-    func dealedCards(_ stud: Int) -> CardStack? {
-        let dealedCards = deck?.drawMany(selectedCount: stud)
-        return dealedCards
+    struct Position {
+        var fetchedCardsPosition: CGPoint
+        var arrangedCardsPosition: CGPoint
+        var spareCardsPosition: CGPoint
+        var revealedCardsPosition: CGPoint
+
+        init(on view: UIView, with cardSize: CGSize) {
+            fetchedCardsPosition = CGPoint(x: view.layoutMargins.left, y: view.layoutMargins.top+cardSize.height+15)
+            arrangedCardsPosition = CGPoint(x: view.layoutMargins.left, y: view.layoutMargins.top)
+            spareCardsPosition =
+                CGPoint(x: view.frame.width-(view.layoutMargins.right+cardSize.width)+Constants.horizontalStackSpacing,
+                        y: view.layoutMargins.top)
+            revealedCardsPosition =
+                CGPoint(x: spareCardsPosition.x-cardSize.width-Constants.horizontalStackSpacing,
+                        y: view.layoutMargins.top)
+        }
     }
 }
 
 extension ViewController {
-    private func dealedCardViewStacks(of maxStud: Int) -> [DealedCardViewStack] {
-        var dealedCardViewStacks: [DealedCardViewStack] = []
-        for stud in 1...Constants.GameBoard.maxStud {
-            let dealedCardViewStack = dealCardViewStack(of: stud)
-            dealedCardViewStacks.append(dealedCardViewStack)
+    private func bindDataWithSpareCard() {
+        // 뷰모델-뷰 바인딩
+        deckViewModel.spareCardDummy.cards.bindAndFire { [unowned self] in
+            if let currCard = $0.last {
+                let cardViewModel = CardViewModel(card: currCard, isFaceUp: false)
+                self.spareCardView.image = cardViewModel.image
+            }
         }
-        return dealedCardViewStacks
     }
 
-    private func dealCardViewStack(of stud: Int) -> DealedCardViewStack {
-        let cardViews = makeCardViews(count: stud)
-        let spacing = -cardSize.height*0.7
-        let cardCount = cardViews.count
-        let bottomMarginToLastCard =
-            (view.frame.height-dealedPosition!.y)-(CGFloat(cardCount-1)*cardSize.height*0.3+cardSize.height)
-        return DealedCardViewStack(frame: .zero, cardViews: cardViews,
-                                   spacing: spacing, bottomMargin: bottomMarginToLastCard)
+    private func bindDataWithRevealedCard() {
+        deckViewModel.revealedCardDummy.cards.bindAndFire { [unowned self] in
+            if let currCard = $0.last {
+                let cardViewModel = CardViewModel(card: currCard, isFaceUp: true)
+                self.revealedCardView.image = cardViewModel.image
+            } else {
+                self.revealedCardView.image = nil
+            }
+        }
     }
 
-    private func makeCardViews(count: Int) -> [CardView] {
-        var cardViews: [CardView] = []
-        for _ in 0..<count {
-            let newCardView = CardView(sizeOf: cardSize)
-            cardViews.append(newCardView)
+    private func bindDataWithArrangedCards() {
+        for (stackIndex, cardStack) in deckViewModel.arrangedCardDummys.enumerated() {
+            cardStack.cards.bindAndFire { [unowned self] in
+                guard let imageView =
+                    self.arrangedCardViews.arrangedSubviews[stackIndex] as? UIImageView else { return }
+                if let currCard = $0.last {
+                    let cardViewModel = CardViewModel(card: currCard, isFaceUp: true)
+                    imageView.image = cardViewModel.image
+                } else {
+                    imageView.image = nil
+                }
+            }
         }
-        return cardViews
+    }
+
+    private func bindDataWithFetchedCards() {
+        for (stackIndex, cardStack) in deckViewModel.fetchedCardDummys.enumerated() {
+            guard let imageViews = self.fetchedStackViews[stackIndex].arrangedSubviews as? [UIImageView] else { break }
+            cardStack.cards.bindAndFire { cards in
+                for (cardIndex, card) in cards.enumerated() {
+                    let cardViewModel = CardViewModel(card: card)
+                    if cardIndex == cards.endIndex-1 {
+                        cardViewModel.turnOver(toFrontFace: true)
+                    }
+                    imageViews[cardIndex].image = cardViewModel.image
+                }
+            }
+        }
     }
 
 }
