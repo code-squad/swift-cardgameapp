30d0e363c51e11b2055e1aa92df38411e25364f2
undervineg <undervineg@gmail.com>
2018-02-11 01:32:53 +0900

    [추가] 뷰모델의 프로퍼티 변화를 감지할 수 있는 클래스 작성

--- CardGameApp/CardGameApp/ViewModel/Observable.swift
+++ CardGameApp/CardGameApp/ViewModel/Observable.swift
@@ -7,3 +7,55 @@
 //
 
 import Foundation
+
+// 원시타입 관찰
+class Observable<ObservingType> {
+    typealias Listener = (ObservingType) -> Void
+    private var listener: Listener?
+    var value: ObservingType {
+        didSet {
+            listener?(value)
+        }
+    }
+
+    required init(_ value: ObservingType) {
+        self.value = value
+    }
+
+    func bind(_ listener: Listener?) {
+        self.listener = listener
+    }
+
+    func bindAndFire(_ listener: Listener?) {
+        self.listener = listener
+        listener?(self.value)
+    }
+}
+
+// 스택과 같이 Sequence 타입 관찰 - 안 됨..
+//class ContainerObserving<ContainerType: Sequence> {
+//    typealias ObservingType = ContainerType.Iterator
+//    typealias Listener = (ObservingType) -> Void
+//    private var listener: Listener?
+//    var value: ObservingType {
+//        didSet {
+//            listener?(value)
+//        }
+//    }
+//    var container: ContainerType
+//
+//    required init(_ container: ContainerType) {
+//        self.container = container
+//        self.value = container.makeIterator()
+//    }
+//
+//    func bind(_ listener: ((ContainerObserving.ObservingType) -> Void)?) {
+//        self.listener = listener
+//    }
+//
+//    func bindAndFire(_ listener: ((ContainerObserving.ObservingType) -> Void)?) {
+//        self.listener = listener
+//        listener?(self.value)
+//    }
+//
+//}
