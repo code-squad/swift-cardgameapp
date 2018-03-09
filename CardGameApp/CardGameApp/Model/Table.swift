c22db944132a9043814c6842da7ff18b8bacfecc
DongJin Lee <comets195@gmail.com>
2018-03-09 10:40:59 +0900

    [수정]Table.swift 코드정리..

--- CardGameApp/CardGameApp/Model/Table.swift
+++ CardGameApp/CardGameApp/Model/Table.swift
@@ -13,11 +13,10 @@ import Foundation
 
 class Table {
     private (set) var cardStacksOfTable: [[Card]]
-    private var deck: Deck
+    private var deck: CardGameInfo
     
-    // 게임의 타입과 플레이어의 숫자에 따라, 각자 가지고있는 카드의 수, 플레이어의 수에 맞추어
     // 테이블을 세팅한다.
-    init(with deck: Deck) {
+    init(with deck: CardGameInfo) {
         self.cardStacksOfTable = [[Card]]()
         self.deck = deck
         self.deck.shuffle()
@@ -25,7 +24,7 @@ class Table {
 
     // gameInfo를 가지고 실제 테이블 세팅을 수행.
     // 테이블 세팅은 하나의 deck을 공용으로 사용한다. 플레이어들에게 분배된 카드를 제외한 나머지 카드를 되돌려줌.
-    func dealTheCardOfGameTable() throws -> Deck {
+    func dealTheCardOfGameTable() throws -> CardGameInfo {
         for index in 1...7 {
             guard let playerStack = try? self.deck.makeStack(numberOfCards: index) else {
                 throw ErrorCode.zeroCard
