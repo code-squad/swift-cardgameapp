fabc96c9f370d3de26a7523ba667c02b2005fe1a
DongJin Lee <comets195@gmail.com>
2018-03-09 10:40:21 +0900

    [수정]Notification 덱에서의 더블탭 라벨 추가. #12

---
+++ CardGameApp/CardGameApp/Utility/NotificationName.swift
@@ -0,0 +1,14 @@
+//
+//  NotificationName.swift
+//  CardGameApp
+//
+//  Created by Mrlee on 2018. 3. 8..
+//  Copyright © 2018년 Napster. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static var doubleTapStack = Notification.Name("doubleTapStack")
+    static var doubleTapOpenedCard = Notification.Name("doubleTapOpenedCard")
+}
