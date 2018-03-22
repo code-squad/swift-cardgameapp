21b521f973b36c27f4d4b99f437645d41ccfa04a
undervineg <undervineg@gmail.com>
2018-03-22 15:14:39 +0900

    [수정] 기존 좌표계산 부분은 gameView로 옮김. spare, refresh, double tap인지에 따라 처리 로직 추가

--- CardGameApp/CardGameApp/GameViewController.swift
+++ CardGameApp/CardGameApp/GameViewController.swift
@@ -12,174 +12,69 @@ class GameViewController: UIViewController {
 
     private(set) var gameView: GameView! {
         didSet {
-            gameView.setup()
             gameView.delegate = self
             view.addSubview(gameView)
         }
     }
-    private var game: GameViewModel!
+    private var gameViewModel: GameViewModel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        gameView = GameView(frame: view.frame)
-        game = GameViewModel()
-        setDefaultTableauViews()
-        bindDataInSpareView()
-        bindDataInWasteView()
-        bindDataInFoundationView()
-//        bindDataInTableauView()
-        for (view, viewModel) in zip(self.gameView.tableauViews, self.game.tableau) {
-            self.bindDataInTableauView(viewModel, in: view)
-        }
+        gameViewModel = GameViewModel()
+        gameView = GameView(frame: view.frame, game: gameViewModel)
+        gameViewModel.initialize()
+        gameView.initialize()
     }
 }
 
-extension GameViewController: CanHandleGesture, CanUpdateViewModel {
-
+extension GameViewController: CardViewActionDelegate {
     override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
         if motion == .motionShake {
-            game.initialize()
-            setDefaultTableauViews()
+            gameViewModel.initialize()
+            gameView.newGame(with: gameViewModel)
         }
     }
 
-    func handleSingleTapOnSpare() {
-        game.updateSpare()
-    }
-
-    func handleDoubleTapOnCard(tappedView: CardView, recognizer: UITapGestureRecognizer) {
-        guard let tappedCard = tappedView.viewModel else { return }
-        if let endLocation = game.canMoveToLocation(tappedCard.card) {
-            let startLocation = tappedCard.location
-            let startContainerOrigin = startLocation.origin(inside: gameView)
-            let endContainerOrigin = endLocation.origin(inside: gameView)
-
-            var startPoint = CGPoint()
-            switch startLocation {
-            case .tableau(let stackNumber):
-                let heightExceptLast =
-                    gameView.tableauViews[stackNumber].nextYPosition - gameView.config.cardSize.height*0.3
-                let y = startContainerOrigin.y + heightExceptLast
-                startPoint = CGPoint(x: startContainerOrigin.x, y: y)
-            case .waste: startPoint = tappedView.convertOrigin(relativeTo: gameView)
-            default: startPoint = startContainerOrigin
-            }
-
-            var endPoint = CGPoint()
-            switch endLocation {
-            case .tableau(let stackNumber):
-                let height = gameView.tableauViews[stackNumber].nextYPosition
-                let y = endContainerOrigin.y + height
-                endPoint = CGPoint(x: endContainerOrigin.x, y: y)
-            default: endPoint = endContainerOrigin
-            }
-
-            let movableCardView = MovableCardView(cardView: tappedView,
-                                                  from: startLocation, to: endLocation,
-                                                  startPoint: startPoint, endPoint: endPoint)
-            movableCardView.delegate = self
-            movableCardView.animateToMove()
-        }
-
-    }
-
-    // 애니메이션 직후 동작 (뷰, 모델변경)
-    func updateMovedCards(movableCardView: MovableCardView) {
-        // 모델 변경
-        game.moveFrontCard(from: movableCardView.fromLocation, to: movableCardView.toLocation) { [unowned self] in
-            // 모델 변경 후, Tableau의 경우 스택뷰에 추가 (애니메이션 시에 추가가 안됐으므로)
-//            if case let Location.tableau(stackNumber) = movableCardView.fromLocation {
-//                self.gameView.tableauViews[stackNumber].removeCard(cardView: movableCardView.cardView)
-//            }
-//            if case let Location.tableau(stackNumber) = movableCardView.toLocation {
-//                self.gameView.tableauViews[stackNumber].appendCard(cardView: movableCardView.cardView)
-//            }
+    func onSpareViewTapped(tappedView: CardView) {
+        guard let tappedCardViewModel = tappedView.viewModel else { return }
+        tappedCardViewModel.turnOver(to: .up)
+        // 탭한 뷰의 적정 위치 찾은 후
+        if let tolocation = gameViewModel.suitableLocation(for: tappedCardViewModel) {
+            // 뷰 업데이트
+            let movableCardView = MovableCardView(cardView: tappedView, endLocation: tolocation)
+            gameView.move(movableCardView)
+            // 뷰 업데이트 후 뷰모델 및 모델 업데이트
+            tappedView.viewModel?.location.value = tolocation
+            gameViewModel.moveToWaste(tappedCardViewModel)
         }
     }
 
-}
-
-extension GameViewController {
-
-    private func bindDataInSpareView() {
-        game.spare.cardViewModels.bindAndFire { [unowned self] in
-            if let viewModel = $0 {
-                self.gameView.spareView.setViewModel(viewModel)
-            } else {
-                let refreshImage = UIImage(imageLiteralResourceName: GameViewModel.Settings.refresh)
-                self.gameView.spareView.setImage(refreshImage)
+    func onRefreshButtonTapped() {
+        for card in gameView.laidCards {
+            guard let cardViewModel = card.viewModel else { break }
+            if case Location.waste = cardViewModel.location.value {
+                card.viewModel?.turnOver(to: .down)
+                let movableCard = MovableCardView(cardView: card, endLocation: .spare)
+                gameView.move(movableCard)
+                // 뷰 업데이트 후 뷰모델 및 모델 업데이트
+                card.viewModel?.location.value = .spare
+                gameViewModel.refreshWaste()
             }
         }
     }
 
-    private func bindDataInWasteView() {
-        // 모델에 추가된 카드($0)가 있으면 뷰모델로 만든 후 뷰에 붙임.
-        // 모델에서 카드가 제거되면 제거된 카드 그림 지우고 뷰에서 떼어냄.
-        game.waste.cardViewModels.bind { [unowned self] in
-            // 새 이미지가 있는 경우, 붙인다.
-            if let viewModel = $0 {
-                self.gameView.wasteView.appendCard(cardViewModel: viewModel)
-            } else {
-                self.gameView.wasteView.eraseAllImages()
-                self.gameView.wasteView.removeAll()
-            }
+    func onCardViewDoubleTapped(tappedView: CardView) {
+        guard let tappedCardViewModel = tappedView.viewModel else { return }
+        let fromLocation = tappedCardViewModel.location.value
+        // 탭한 뷰의 적정 위치 찾은 후
+        if let location = gameViewModel.suitableLocation(for: tappedCardViewModel) {
+            // 뷰 업데이트
+            let movableCardView = MovableCardView(cardView: tappedView, endLocation: location)
+            gameView.move(movableCardView)
+            // 뷰 업데이트 후 뷰모델 및 모델 업데이트
+            tappedView.viewModel?.location.value = location
+            gameViewModel.move(cardViewModel: tappedCardViewModel, from: fromLocation, to: location)
         }
     }
 
-    private func bindDataInFoundationView() {
-        // index: 0...3
-        for (index, (view, viewModel)) in zip(gameView.foundationView.arrangedSubviews, game.foundation).enumerated() {
-            switch viewModel.spaceState {
-            case .exist(let card):
-                card.bindAndFire {
-                    let cardViewModel =
-                        CardViewModel(card: $0, faceState: .up, borderState: .show, location: .foundation(index))
-                    (view as? CardView)?.setViewModel(cardViewModel)
-                }
-            case .vacant: break
-            }
-        }
-    }
-
-    // 각 Tableau 스택의 데이터 바인딩.
-    private func bindDataInTableauView(_ tableauViewModel: TableauViewModel, in stackView: TableauView) {
-        tableauViewModel.cardViewModels.bind {
-            // $0 = 현재 모델의 프론트(마지막) 카드.
-            guard let isAdded = tableauViewModel.isAdded else { return }
-            if isAdded {
-                // 추가 - 앞면인 상태로 붙임
-//                $0?.turnOver(toFace: .up)
-                stackView.appendCard(cardViewModel: $0)
-            } else {
-                // 제거 - 제거 후 마지막 카드 뒤집음
-                stackView.removeCards(count: 1)
-                $0?.turnOver(toFace: .up)
-            }
-        }
-    }
-
-    // 맨 초기 tableau 카드 뷰에 데이터 삽입.
-    func setDefaultTableauViews() {
-        // index: 0...6
-        for (stackView, tableauViewModel) in zip(gameView.tableauViews, game.tableau) {
-            stackView.removeAllCards()
-            tableauViewModel.turnOverLastCard(to: .up)
-            tableauViewModel.cardViewModels.collection.forEach {
-                stackView.appendCard(cardViewModel: $0)
-            }
-        }
-    }
-
-//    func bindWithTableauView() {
-//        // index: 0...6
-//        for (stackView, tableauViewModel) in zip(gameView.tableauViews, game.tableau) {
-//            tableauViewModel.cardViewModels.bindAndFire { _ in
-//                stackView.removeAllCards()
-//                tableauViewModel.turnOverLastCard(to: .up)
-//                tableauViewModel.cardViewModels.collection.forEach {
-//                    stackView.appendCard(cardViewModel: $0)
-//                }
-//            }
-//        }
-//    }
 }
