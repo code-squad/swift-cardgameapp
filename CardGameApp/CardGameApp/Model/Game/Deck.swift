5132d045b096114ac775a7798504c6cb4ee1a0a6
undervineg <undervineg@gmail.com>
2018-03-22 14:19:27 +0900

    [수정] 변수명 수정 및 fetch() 메소드 반환타입 변경

--- CardGameApp/CardGameApp/Model/Game/Deck.swift
+++ CardGameApp/CardGameApp/Model/Game/Deck.swift
@@ -10,7 +10,7 @@ import Foundation
 
 class Deck: Sequence {
     // 덱에 있는 카드들. (52장)
-    private var cards: CardStack!
+    private var cardStack: CardStack!
     let start: Int = 0
 
     init() {
@@ -18,22 +18,22 @@ class Deck: Sequence {
     }
 
     func makeIterator() -> ClassIteratorOf<Card> {
-        return ClassIteratorOf(self.cards.sorted())
+        return ClassIteratorOf(self.cardStack.sorted())
     }
 
     // 여러 장의 카드를 스택으로 반환.
-    func fetch(_ count: Int) -> CardStack? {
-        guard self.cards.count - count > 0 else { return nil }
+    func fetch(_ count: Int) -> [Card]? {
+        guard self.cardStack.count - count > 0 else { return nil }
         let removedCards = CardStack()
         for _ in 0..<count {
-            guard let popedCard = self.cards.pop() else { break }
+            guard let popedCard = self.cardStack.pop() else { break }
             removedCards.push(card: popedCard)
         }
-        return removedCards
+        return removedCards.cards.collection
     }
 
-    func remnants() -> CardStack? {
-        return self.cards
+    func remnants() -> [Card]? {
+        return self.cardStack.cards.collection
     }
 
 }
@@ -41,28 +41,28 @@ class Deck: Sequence {
 // 덱도 카드스택의 일종.
 extension Deck: CardGameStack {
     var count: Int {
-        return self.cards.count
+        return self.cardStack.count
     }
     var isEmpty: Bool {
-        return self.cards.isEmpty
+        return self.cardStack.isEmpty
     }
 
     func pop() -> Card? {
-        return self.cards.pop()
+        return self.cardStack.pop()
     }
 
     func push(card: Card) {
         // 52장보다 적은 경우에만 넣을 수 있음.
-        guard self.cards.count < 52 else { return }
-        self.cards.push(card: card)
+        guard self.cardStack.count < 52 else { return }
+        self.cardStack.push(card: card)
     }
 
     func peek() -> Card? {
-        return self.cards.peek()
+        return self.cardStack.peek()
     }
 
     func shuffle() {
-        self.cards.shuffle()
+        self.cardStack.shuffle()
     }
 
     // 정렬된 카드 52장 새로 생성.
@@ -76,7 +76,7 @@ extension Deck: CardGameStack {
                 aStackOfCards.push(card: Card(shape, number))
             }
         }
-        self.cards = aStackOfCards
+        self.cardStack = aStackOfCards
     }
 
 }
