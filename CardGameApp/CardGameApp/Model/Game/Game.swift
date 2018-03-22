6282b8110e910196d33eead6eee32047e88d9ffe
undervineg <undervineg@gmail.com>
2018-03-22 14:29:16 +0900

    [추가] 카드 덱 셔플 및 영역별 분배, 적합한 위치 선정 및 이동 메소드 추가

--- CardGameApp/CardGameApp/Model/Game/Game.swift
+++ CardGameApp/CardGameApp/Model/Game/Game.swift
@@ -7,3 +7,85 @@
 //
 
 import Foundation
+
+class Game {
+    private var deck: Deck {
+        didSet {
+            spare.reset(with: deck.remnants()!)
+        }
+    }
+    private(set) var foundations: [Foundation]
+    private(set) var waste: CardStack
+    private(set) var spare: Spare
+    private(set) var tableaus: [Tableau]
+    private let tableauConfig: [Int] = [1, 2, 3, 4, 5, 6, 7]
+
+    init() {
+        deck = Deck()
+        foundations = (0..<4).map { _ in Foundation() }
+        waste = CardStack()
+        spare = Spare()
+        tableaus = (0..<7).map { _ in Tableau() }
+    }
+
+    func new() {
+        deck.reset()
+        deck.shuffle()
+        foundations.forEach { $0.reset() }
+        waste.reset()
+        (1...7).forEach {
+            guard let fetchedCards = deck.fetch($0) else { return }
+            tableaus[$0-1].reset(with: fetchedCards)
+        }
+        spare.reset(with: deck.remnants()!)
+    }
+
+    // waste -> spare
+    func refreshWaste() {
+        spare.push(waste.cards.collection)
+    }
+
+    func suitableLocation(_ card: Card) -> Location? {
+        for (index, foundation) in foundations.enumerated() where foundation.canPush(card) {
+            return Location.foundation(index)
+        }
+        for (index, tableau) in tableaus.enumerated() where tableau.canPush(below: card) {
+            return Location.tableau(index)
+        }
+        return nil
+    }
+
+    /**
+     * spare -> waste
+     * waste -> tableau, foundation
+     * foundation -> tableau
+     * tableau -> tableau, foundation
+     **/
+    func move(cardsFrom card: Card, from fromLocation: Location, to toLocation: Location) {
+        var movingCards: [Card] = []
+
+        switch fromLocation {
+        case .spare:
+            guard let lastCard = spare.pop() else { break }
+            movingCards.append(lastCard)
+        case .waste:
+            guard let lastCard = waste.pop() else { break }
+            movingCards.append(lastCard)
+        case .foundation(let index):
+            guard let lastCard = foundations[index].pop() else { break }
+            movingCards.append(lastCard)
+        case .tableau(let index):
+            movingCards = tableaus[index].popCards(below: card)
+        }
+
+        switch toLocation {
+        case .spare: break
+        case .waste: waste.push(card: card)
+        case .foundation(let index) where foundations[index].canPush(card):
+            foundations[index].push(card: card)
+        case .tableau(let index) where tableaus[index].canPush(below: card):
+            tableaus[index].push(cards: movingCards.flatMap { $0 })
+        default: break
+        }
+    }
+}
