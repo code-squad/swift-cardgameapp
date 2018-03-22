e9380a93fb4c252fef83fdd7f04fc6107bb5d0f9
undervineg <undervineg@gmail.com>
2018-03-22 15:05:33 +0900

    [수정] 소유한 카드뷰모델의 상태에 따라 뷰에 표시하는 역할만 하도록 수정
    - 기존 -set, -remove 등의 변경 메소드 제거
    + 기존에는 현재 카드의 location에 따라 탭 제스처인식기를 다르게 붙였으나,(spare인 경우 탭 1번) 카드 위치가 자주 변경되므로 모두 붙이도록 수정

--- CardGameApp/CardGameApp/View/CardView.swift
+++ CardGameApp/CardGameApp/View/CardView.swift
@@ -9,120 +9,38 @@
 import UIKit
 
 class CardView: UIImageView {
-    private(set) var viewModel: CardViewModel? {
-        didSet {
-            self.image = viewModel?.image
-            registerGesture()
+
+    private(set) var viewModel: CardViewModel?
+
+    convenience init(viewModel: CardViewModel, frame: CGRect) {
+        self.init(frame: frame)
+        self.viewModel = viewModel
+        self.image = viewModel.onCurrentFace()
+        registerTapGesture(tapCount: 1)
+        registerTapGesture(tapCount: 2)
+        viewModel.status.bind { [unowned self] _ in
+            self.image = viewModel.onCurrentFace()
         }
     }
-    private var borderState: BorderState = .show
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.layer.cornerRadius = cornerRadius
         self.clipsToBounds = clipToBounds
-        self.sendBack()
-    }
-
-    func bringFront() {
-        self.layer.zPosition = .infinity
-    }
-
-    func sendBack() {
-        self.layer.zPosition = 1
-    }
-
-    convenience init(viewModel: CardViewModel, size: CGSize) {
-        self.init(image: viewModel.image, faceState: viewModel.faceState, size: size, borderState: viewModel.borderState)
-        self.viewModel = viewModel
-        registerGesture()
-    }
-
-    private func registerGesture() {
-        guard let viewModel = viewModel else { return }
-        // 현재 카드 위치에 따라 다른 제스처 등록.
-        switch viewModel.location {
-        case .spare: registerTapGesture(tapCount: 1)
-        default: registerTapGesture(tapCount: 2)
-        }
-    }
-
-    convenience init(frame: CGRect, borderState: BorderState) {
-        self.init(frame: frame)
-        setSizeTo(frame.size)
-        setDefaultBorderStyle(borderState: borderState)
-        self.borderState = borderState
-    }
-
-    convenience init(size: CGSize, borderState: BorderState) {
-        self.init(frame: CGRect(origin: .zero, size: size), borderState: borderState)
-    }
-
-    convenience init(image: UIImage, faceState: FaceState, size: CGSize, borderState: BorderState) {
-        self.init(size: size, borderState: borderState)
-        self.image = image
-    }
-
-    convenience init() {
-        self.init(frame: .zero)
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    func setBorderState(to borderState: BorderState) {
-        self.borderState = borderState
+    func bringToFront() {
+        superview?.bringSubview(toFront: self)
     }
 
-    func setViewModel(_ viewModel: CardViewModel) {
-        self.viewModel = viewModel
-        self.image = viewModel.image
+    func sendBack() {
+        superview?.sendSubview(toBack: self)
     }
 
-    func setImage(_ image: UIImage) {
-        self.image = image
-    }
-
-    func removeImage() {
-        self.image = nil
-    }
-
-}
-
-extension CardView {
-    private func registerTapGesture(tapCount: Int) {
-        self.isUserInteractionEnabled = true
-        let selector = (tapCount == 1) ? #selector(handleSingleTap(sender:)) : #selector(handleDoubleTap(sender:))
-        let recognizer = UITapGestureRecognizer(target: self, action: selector)
-        recognizer.numberOfTapsRequired = tapCount
-        self.addGestureRecognizer(recognizer)
-    }
-
-    @objc func handleSingleTap(sender: UITapGestureRecognizer) {
-        handleCertainView { gameView in
-            gameView.handleSingleTapOnSpare()
-        }
-    }
-
-    @objc func handleDoubleTap(sender: UITapGestureRecognizer) {
-        // 앞면인 경우에만 터치 동작.
-        guard self.viewModel?.faceState == FaceState.up else { return }
-        handleCertainView { gameView in
-            gameView.handleDoubleTapOnCard(tappedView: self, recognizer: sender)
-        }
-    }
-
-    private func handleCertainView(execute: (GameView) -> Void) {
-        // GameView까지 올라가서 핸들러 작동.
-        var nextResponder = self.next
-        while nextResponder != nil {
-            if let gameView = nextResponder as? GameView {
-                execute(gameView)
-            }
-            nextResponder = nextResponder?.next
-        }
-    }
 }
 
 extension CardView: CardPresentable {
@@ -139,6 +57,44 @@ extension CardView: CardPresentable {
             self.layer.borderColor = borderColor
             self.layer.borderWidth = borderWidth
         default: break
+        }
+    }
+}
+
+extension CardView {
+
+    private func registerTapGesture(tapCount: Int) {
+        let selector = (tapCount == 1) ? #selector(handleSingleTap(sender:)) : #selector(handleDoubleTap(sender:))
+        let recognizer = UITapGestureRecognizer(target: self, action: selector)
+        recognizer.numberOfTapsRequired = tapCount
+        self.isUserInteractionEnabled = true
+        self.addGestureRecognizer(recognizer)
+    }
+
+    @objc func handleSingleTap(sender: UITapGestureRecognizer) {
+        guard let currLocation = self.viewModel?.location.value, case Location.spare = currLocation else { return }
+        handleCertainView { gameView in
+            self.bringToFront()
+            gameView.delegate?.onSpareViewTapped(tappedView: self)
+        }
+    }
+
+    @objc func handleDoubleTap(sender: UITapGestureRecognizer) {
+        guard viewModel!.isUserInteractive else { return }
+        handleCertainView { gameView in
+            self.bringToFront()
+            gameView.delegate?.onCardViewDoubleTapped(tappedView: self)
+        }
+    }
+
+    private func handleCertainView(execute: (GameView) -> Void) {
+        // GameView까지 올라가서 핸들러 작동
+        var nextResponder = self.next
+        while nextResponder != nil {
+            if let gameView = nextResponder as? GameView {
+                execute(gameView)
+            }
+            nextResponder = nextResponder?.next
         }
     }
 }
