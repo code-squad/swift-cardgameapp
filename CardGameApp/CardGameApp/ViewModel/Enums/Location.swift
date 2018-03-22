f6e1b0bd8d12aa0e2e2a31aa498d94493aabd4c9
undervineg <undervineg@gmail.com>
2018-03-22 14:35:57 +0900

    [제거] location별 좌표값을 반 반환하는 origin() 메소드 제거

--- CardGameApp/CardGameApp/ViewModel/Enums/Location.swift
+++ CardGameApp/CardGameApp/ViewModel/Enums/Location.swift
@@ -14,31 +14,3 @@ enum Location {
     case foundation(Int)
     case tableau(Int)
 }
-
-extension Location {
-    func origin(inside view: UIView) -> CGPoint {
-        guard let gameView = view as? GameView else { return CGPoint() }
-        var origin = CGPoint()
-        switch self {
-        case .foundation(let spaceNumber):
-            let stackX = gameView.foundationView.arrangedSubviews[spaceNumber].frame.origin.x
-            let stackY = gameView.foundationView.arrangedSubviews[spaceNumber].convertOrigin(relativeTo: gameView).y
-            origin = CGPoint(x: stackX, y: stackY)
-        case .tableau(let stackNumber):
-            let stackX = gameView.tableauViews[stackNumber].frame.origin.x
-            let stackY = gameView.tableauViews[stackNumber].convertOrigin(relativeTo: gameView).y
-            origin = CGPoint(x: stackX, y: stackY)
-        case .spare:
-            origin = gameView.spareView.convertOrigin(relativeTo: gameView)
-        case .waste:
-            origin = gameView.wasteView.convertOrigin(relativeTo: gameView)
-        }
-        return origin
-    }
-}
-
-extension UIView {
-    func convertOrigin(relativeTo view: UIView) -> CGPoint {
-        return self.convert(self.frame.origin, to: view)
-    }
-}
