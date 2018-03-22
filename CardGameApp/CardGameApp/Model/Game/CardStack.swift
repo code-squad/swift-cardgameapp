b50f2a91a67099a4e02455037afa54962a14ee4d
undervineg <undervineg@gmail.com>
2018-03-22 14:23:18 +0900

    [추가] cards를 ObservableCollection타입으로 래핑하여 변경 시 알릴 수 있도록 함

--- CardGameApp/CardGameApp/Model/Game/CardStack.swift
+++ CardGameApp/CardGameApp/Model/Game/CardStack.swift
@@ -10,52 +10,58 @@ import Foundation
 
 // 특정 개수의 카드 뭉치 클래스.
 class CardStack: Sequence {
-    private(set) var cards: [Card]
+    private(set) var cards: ObservableCollection<[Card]>
     let start: Int = 0
+    private var prevCount: Int = 0
     init() {
-        self.cards = []
+        self.cards = ObservableCollection([])
     }
 
     init(_ cards: [Card]) {
-        self.cards = cards
+        self.cards = ObservableCollection(cards)
     }
 
     // 내부 카드들에 접근 가능.
     func makeIterator() -> ClassIteratorOf<Card> {
-        return ClassIteratorOf(self.cards)
+        return ClassIteratorOf(self.cards.collection)
     }
 }
 
 extension CardStack: CardGameStack {
-    var count: Int {
-        return self.cards.count
+    var isAdded: Bool {
+        return (cards.prevCount < count) ? true : false
     }
+
+    var count: Int {
+        return self.cards.collection.count
+    }
+
     var isEmpty: Bool {
-        return self.cards.isEmpty
+        return self.cards.collection.isEmpty
     }
 
     func push(card: Card) {
-        self.cards.append(card)
+        self.cards.collection.append(card)
     }
 
     func pop() -> Card? {
-        return self.cards.popLast()
+        return self.cards.collection.popLast()
     }
 
     func peek() -> Card? {
-        return self.cards.last
+        return self.cards.collection.last
     }
 
     func shuffle() {
-        guard let shuffledCards = self.cards.shuffle() else { return }
-        self.cards = shuffledCards
+        guard let shuffledCards = self.cards.collection.shuffle() else { return }
+        self.cards = ObservableCollection(shuffledCards)
     }
 
     func reset() {
-        self.cards = []
+        self.cards = ObservableCollection([])
     }
 
     var endIndex: Int {
-        return self.cards.endIndex
+        return self.cards.collection.endIndex
     }
 }
