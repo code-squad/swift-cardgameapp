333e0d24483afc3816becaad044ea6951639ae18
undervineg <undervineg@gmail.com>
2018-03-22 15:00:03 +0900

    [추가] 카드가 없는 빈 영역을 나타낼 뷰 클래스 추가

--- CardGameApp/CardGameApp/View/EmptyView.swift
+++ CardGameApp/CardGameApp/View/EmptyView.swift
@@ -6,4 +6,25 @@
 //  Copyright © 2018년 심 승민. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class EmptyView: UIImageView, CardPresentable {
+
+    convenience init(frame: CGRect, hasBorder: Bool) {
+        self.init(frame: frame)
+        self.layer.borderWidth = (hasBorder) ? 2 : 0
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.layer.borderColor = UIColor.white.cgColor
+        self.layer.borderWidth = 2
+        self.layer.cornerRadius = cornerRadius
+        self.clipsToBounds = clipToBounds
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
