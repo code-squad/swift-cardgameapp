a84277179b5ffde95b7604da5c343e0d4253aa25
undervineg <undervineg@gmail.com>
2018-03-24 17:00:48 +0900

    [이동] 영역별 뷰에 카드를 추가/제거하기 위한 프로토콜인 CanLayCards 파일을 따로 만듦

---
+++ CardGameApp/CardGameApp/View/Protocols/CanLayCards.swift
@@ -0,0 +1,14 @@
+//
+//  CanLayCards.swift
+//  CardGameApp
+//
+//  Created by 심 승민 on 2018. 3. 24..
+//  Copyright © 2018년 심 승민. All rights reserved.
+//
+
+import Foundation
+
+protocol CanLayCards {
+    func lay(card: CardView)
+    func removeLastCard()
+}
