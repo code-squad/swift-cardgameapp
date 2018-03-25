69ba514513306cd293867f9c4a9ab076c9cf766e
undervineg <undervineg@gmail.com>
2018-03-26 01:28:48 +0900

    [수정] 애니메이션 처리 클래스를 UIView로 만들고, 카드 여러 장을 묶어 이동할 수 있도록 함
    클릭한 카드와 밑에 딸린 카드들을 하나로 묶어 frame 설정하고, addSubview()를 통해 하위 뷰로 붙임
    애니메이션 후 하위 뷰들을 떼어내어 gameView에 붙임

---
+++ CardGameApp/CardGameApp/View/MovableCardView.swift
@@ -0,0 +1,131 @@
+//
+//  MovableCardView.swift
+//  CardGameApp
+//
+//  Created by 심 승민 on 2018. 2. 14..
+//  Copyright © 2018년 심 승민. All rights reserved.
+//
+
+import UIKit
+
+class MovableCardView: UIView, CanFindGameView {
+    weak var delegate: UpdateModelDelegate?
+    private var cardView: CardView
+    private var cardViewsBelow: [CardView]?
+    private var startPosition: CGPoint = .zero
+    private var fromLocation: Location
+    private(set) var endLocation: Location
+
+    init(cardView: CardView, cardViewsBelow: [CardView]?, endLocation: Location) {
+        self.cardView = cardView
+        self.cardViewsBelow = cardViewsBelow
+        self.fromLocation = cardView.viewModel!.location.value
+        self.endLocation = endLocation
+        self.startPosition = cardView.frame.origin
+
+        let wholeHeight = (cardViewsBelow == nil) ? cardView.frame.height : cardView.frame.height+cardView.frame.height*0.3*(CGFloat(cardViewsBelow!.count)-1)
+        let wholeSize = CGSize(width: cardView.frame.width, height: wholeHeight)
+        super.init(frame: CGRect(origin: startPosition, size: wholeSize))
+
+        addSubview(cardView)
+        cardViewsBelow?.forEach { addSubview($0) }
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        self.cardView = CardView(frame: .zero)
+        self.fromLocation = .spare
+        self.endLocation = .spare
+        super.init(coder: aDecoder)
+    }
+
+    override func layoutSubviews() {
+        self.cardView.frame.origin = self.frame.origin
+        self.cardViewsBelow?.enumerated().forEach {
+            $0.element.frame.origin =
+                CGPoint(x: self.cardView.frame.origin.x,
+                        y: self.cardView.frame.origin.y+CGFloat($0.offset+1)*cardView.frame.height*0.3)
+        }
+
+        self.cardView.removeFromSuperview()
+        self.superview?.addSubview(self.cardView)
+        self.cardViewsBelow?.forEach {
+            $0.removeFromSuperview()
+            self.superview?.addSubview($0)
+        }
+        self.removeFromSuperview()
+
+        super.layoutSubviews()
+    }
+
+    func animateToMove(to endPosition: CGPoint?) {
+        guard let endPosition = endPosition else { return }
+
+        let translateTransform = self.transform.translatedBy(x: endPosition.x-startPosition.x,
+                                                             y: endPosition.y-startPosition.y)
+
+        let option = properOption()
+
+        UIView.transition(with: self, duration: 0.3, options: option, animations: {
+            self.bringToFront()
+            self.transform = translateTransform
+            self.layoutIfNeeded()
+        }, completion: { _ in
+            self.updateSuperview()
+            self.updateModel()
+        })
+
+    }
+
+    // MARK: - PRIVATE
+
+    func bringToFront() {
+        superview?.bringSubview(toFront: self)
+    }
+
+    private func properOption() -> UIViewAnimationOptions {
+        // waste->spare 또는 spare->waste 처럼 뒤집어야 하는 경우 옵션 설정
+        var option: UIViewAnimationOptions = .curveEaseIn
+        if case Location.spare = cardView.viewModel!.location.value {
+            option = .transitionFlipFromRight
+        } else if case Location.waste = cardView.viewModel!.location.value, case Location.spare = endLocation {
+            option = .transitionFlipFromLeft
+        }
+        return option
+    }
+
+    private func updateSuperview() {
+        // 카드뷰가 속한 상위뷰 업데이트 (실제로는 gameView의 subview 임)
+        self.handleCertainView(from: self.cardView, execute: { gameView in
+            var endView: CanLayCards
+            switch self.endLocation {
+            case .spare: endView = gameView.spareView
+            case .waste: endView = gameView.wasteView
+            case .foundation(let index): endView = gameView.foundationViewContainer.at(index)
+            case .tableau(let index): endView = gameView.tableauViewContainer.at(index)
+            }
+            self.cardView.move(toView: endView)
+            self.cardViewsBelow?.forEach {
+                $0.move(toView: endView)
+            }
+        })
+    }
+
+    private func updateModel() {
+        // 모델 업데이트
+        let prevEndLocation = self.endLocation
+        switch self.endLocation {
+        case .spare: self.delegate?.refreshWaste()
+        default:
+            self.delegate?.move(cardViewModel: self.cardView.viewModel!,
+                                     from: self.fromLocation, to: self.endLocation)
+            self.cardViewsBelow?.forEach { [unowned self] in
+                self.delegate?.move(cardViewModel: $0.viewModel!, from: self.fromLocation, to: self.endLocation)
+            }
+        }
+        // 모델 업데이트 후, 카드 뷰모델의 Location 데이터 업데이트
+        self.delegate?.update(cardViewModel: self.cardView.viewModel!, to: prevEndLocation)
+        self.cardViewsBelow?.forEach { [unowned self] in
+            self.delegate?.update(cardViewModel: $0.viewModel!, to: prevEndLocation)
+        }
+    }
+}
