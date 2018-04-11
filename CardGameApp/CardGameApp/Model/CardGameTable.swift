595c10b86c769f14f1c5d8b40209d8f570ae08bc
Jaewook Kwon <somedd@naver.com>
2018-04-11 13:08:47 +0900

    Delete useless word(self) in cardTable. #10

--- CardGameApp/CardGameApp/Model/CardGameTable.swift
+++ CardGameApp/CardGameApp/Model/CardGameTable.swift
@@ -18,22 +18,22 @@ protocol TableControl {
 struct CardGameTable : TableControl {
     private var foundation : [Card] = [] {
         didSet {
-            NotificationCenter.default.post(name: .foundation, object: self, userInfo: [Key.Observer.foundation.name : self.foundation])
+            NotificationCenter.default.post(name: .foundation, object: self, userInfo: [Key.Observer.foundation.name : foundation])
         }
     }
     private var cardStacks : [[Card]] = [] {
         didSet {
-            NotificationCenter.default.post(name: .cardStacks, object: self, userInfo: [Key.Observer.cardStacks.name : self.cardStacks])
+            NotificationCenter.default.post(name: .cardStacks, object: self, userInfo: [Key.Observer.cardStacks.name : cardStacks])
         }
     }
     private var openedCard : Card = Card(.spade, .two)
     
     mutating func setOpenedCard(_ card: Card) {
-        self.openedCard = card
+        openedCard = card
     }
     
     mutating func setCardStacks(_ cardStacks: [[Card]]) {
-        self.cardStacks = cardStacks
+        cardStacks = cardStacks
     }
     
 }
@@ -44,22 +44,22 @@ extension CardGameTable {
     mutating func moveCard(_ cardInfo: CardInfo) -> (isTrue : Bool, doubleTappedCard : Card) {
         let doubleTappedCard: Card
         if cardInfo.position == .top {
-            doubleTappedCard = self.openedCard
+            doubleTappedCard = openedCard
             return (checkMove(doubleTappedCard, cardInfo), doubleTappedCard)
         }
-        doubleTappedCard = self.cardStacks[cardInfo.indexOfCard][cardInfo.stackIndex]
+        doubleTappedCard = cardStacks[cardInfo.indexOfCard][cardInfo.stackIndex]
         return (checkMove(doubleTappedCard, cardInfo), doubleTappedCard)
     }
     
     func getCardInfo(_ card: Card) -> CardInfo {
         var cardInfo : CardInfo!
-        for index in self.foundation.indices {
-            guard card == self.foundation[index] else { continue }
+        for index in foundation.indices {
+            guard card == foundation[index] else { continue }
             cardInfo = CardInfo(index, Key.Card.noStack.count, .top)
             break
         }
-        for index in self.cardStacks.indices {
-            guard let stackIndex = self.cardStacks[index].index(where: {$0 == card}) else { continue }
+        for index in cardStacks.indices {
+            guard let stackIndex = cardStacks[index].index(where: {$0 == card}) else { continue }
             cardInfo = CardInfo(index, stackIndex, .cardStacks)
             break
         }
@@ -75,41 +75,41 @@ extension CardGameTable {
     }
     
     mutating private func moveFoundation(_ card: Card, _ cardInfo: CardInfo) -> Bool {
-        for index in self.foundation.indices {
-            guard self.foundation[index].isSameSuit(card) && card.isNextRank(self.foundation[index]) else { continue }
-            self.foundation[index] = card
+        for index in foundation.indices {
+            guard foundation[index].isSameSuit(card) && card.isNextRank(foundation[index]) else { continue }
+            foundation[index] = card
             guard cardInfo.position == .cardStacks else { return true }
-            self.cardStacks[cardInfo.indexOfCard].removeLast()
+            cardStacks[cardInfo.indexOfCard].removeLast()
             return true
         }
         return false
     }
     
     mutating private func moveStack(_ card: Card, _ cardInfo: CardInfo) -> Bool {
-        for index in self.cardStacks.indices {
-            guard let lastCard = self.cardStacks[index].last else { return false }
+        for index in cardStacks.indices {
+            guard let lastCard = cardStacks[index].last else { return false }
             guard card.isDifferentColor(lastCard) && lastCard.isNextRank(card) else { continue }
-            self.cardStacks[index].append(card)
+            cardStacks[index].append(card)
             guard cardInfo.position == .cardStacks else { return true }
-            self.cardStacks[cardInfo.indexOfCard].removeLast()
+            cardStacks[cardInfo.indexOfCard].removeLast()
             return true
         }
         return false
     }
     
     mutating private func moveAce(_ card: Card, _ cardInfo: CardInfo) -> Bool {
-        self.foundation.append(card)
+        foundation.append(card)
         guard cardInfo.position == .cardStacks else { return true }
-        self.cardStacks[cardInfo.indexOfCard].removeLast()
+        cardStacks[cardInfo.indexOfCard].removeLast()
         return true
     }
     
     mutating private func moveKing(_ card: Card, _ cardInfo: CardInfo) -> Bool {
-        for index in self.cardStacks.indices {
-            guard self.cardStacks[index].count == 0 else { continue }
-            self.cardStacks[index].append(card)
+        for index in cardStacks.indices {
+            guard cardStacks[index].count == 0 else { continue }
+            cardStacks[index].append(card)
             guard cardInfo.position == .cardStacks else { return true }
-            self.cardStacks[cardInfo.indexOfCard].removeLast()
+            cardStacks[cardInfo.indexOfCard].removeLast()
             return true
         }
         return false
