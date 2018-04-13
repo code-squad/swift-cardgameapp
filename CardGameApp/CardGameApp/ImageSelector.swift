98c1c64dc8458a0645c7bd922899d1d7f18d78a7
Mijin Jeon <mayihelpu27@gmail.com>
2018-04-13 14:14:34 +0900

    Feat: Add feature- Card has cardImage file name(Resolve #3) - Add ImageSelector protocol - Card class implements ImageSelector

---
+++ CardGameApp/CardGameApp/ImageSelector.swift
@@ -0,0 +1,16 @@
+//
+//  ImageSelector.swift
+//  CardGameApp
+//
+//  Created by YOUTH2 on 2018. 4. 13..
+//  Copyright © 2018년 JINiOS. All rights reserved.
+//
+
+import Foundation
+
+protocol ImageSelector {
+    var frontImage: String { get }
+    var backImage: String { get }
+    var Image: String { get }
+}
+
