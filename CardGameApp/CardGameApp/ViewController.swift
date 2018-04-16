e9a4eff42ab5e0ec3cffb524eb955a5ab9b88bd2
Jung Kim <godrm77@gmail.com>
2018-04-16 14:10:10 +0900

    Revert "Feat: Draw foundations and cardStack on the top of the view #4 - Add variables for setting subView's frames - Modify codes that set up the attribute of cardImages"
    This reverts commit 812e6c8c2626903fd33bd8d8dd2d62cbdbf89ac8.

--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -10,34 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let widthDivider: CGFloat = 8
-    let cardHeightRatio: CGFloat = 1.27
-    let blankPositionY: CGFloat = 20
-    let cardPositionY: CGFloat = 100
-
-    var cardWidth: CGFloat {
-        return self.view.frame.width / widthDivider
-    }
-
-    var cardSize: CGSize {
-        return CGSize(width: self.cardWidth,
-                      height: cardWidth * cardHeightRatio)
-    }
-
-    var space: CGFloat {
-        return cardWidth / 8
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg_pattern")!)
         self.drawCards()
-        self.drawStack()
-        self.drawFoundations()
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
@@ -45,38 +28,23 @@ class ViewController: UIViewController {
     }
 
     private func drawCards() {
+        let cardWidth = self.view.frame.width / 8
+        let cardHeightRatio: CGFloat = 1.27
+        let cardY: CGFloat = 50
+        let space = cardWidth / 8
+
         for i in 0...6 {
             let cardX = (CGFloat(i+1)*space) + (CGFloat(i) * cardWidth)
-            let cardImage = CardImageView()
-            cardImage.getBackSide()
-            cardImage.frame = CGRect(origin: CGPoint(x: cardX, y: cardPositionY),size: self.cardSize)
-            print(CGPoint(x: cardX, y: cardPositionY))
-            self.view.addSubview(cardImage)
+
+            let cardBackImage = CardImageView().getBackImage()
+            cardBackImage.frame = CGRect(x: cardX,
+                                         y: cardY,
+                                         width: cardWidth,
+                                         height: cardWidth * cardHeightRatio)
+            self.view.addSubview(cardBackImage)
         }
     }
 
-    private func drawFoundations() {
-        for i in 0..<4 {
-            let cardX = (CGFloat(i+1)*space) + (CGFloat(i) * cardWidth)
-            let foundation = UIView(frame: CGRect(origin: CGPoint(x: cardX, y: blankPositionY),
-                                             size: self.cardSize))
-            foundation.clipsToBounds = true
-            foundation.layer.cornerRadius = 5.0
-            foundation.layer.borderColor = UIColor.white.cgColor
-            foundation.layer.borderWidth = 1.0
-            self.view.addSubview(foundation)
-        }
-
-    }
-
-    private func drawStack() {
-        let upperRightCornerX = 355.78125
-        let upperRightCornerY = 20.0
-        let stackImage = CardImageView(frame: CGRect(origin: CGPoint(x: upperRightCornerX, y: upperRightCornerY),
-                                                     size: self.cardSize))
-        stackImage.getBackSide()
-        self.view.addSubview(stackImage)
-    }
 
 }
 
