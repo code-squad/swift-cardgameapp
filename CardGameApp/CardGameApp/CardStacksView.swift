0a18350071f15ad2a508679254a5d9b30995403c
Mijin Jeon <mayihelpu27@gmail.com>
2018-04-19 19:37:17 +0900

    Feat: Add UIView for handling cardStacks views #8

--- CardGameApp/CardGameApp/CardStacksView.swift
+++ CardGameApp/CardGameApp/CardStacksView.swift
@@ -10,12 +10,30 @@ import UIKit
 
 class CardStacksView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    private var stackImages: [[CardImageView]] = []
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.isUserInteractionEnabled = true
+    }
+
+    convenience init(stackImages: [[CardImageView]]) {
+        self.init(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        self.stackImages = stackImages
+        drawStacks()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+
+    func drawStacks() {
+        for i in 0..<stackImages.count {
+            for j in 0..<stackImages[i].count {
+                addSubview(stackImages[i][j])
+            }
+        }
+    }
 }
+
