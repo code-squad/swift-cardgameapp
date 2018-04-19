147aad41c1d12d3d4438d77498dfd800644e01b7
Jaewook Kwon <somedd@naver.com>
2018-04-19 10:30:05 +0900

    Add dragCard method. #12

--- CardGameApp/CardGameApp/Model/CardGameTable.swift
+++ CardGameApp/CardGameApp/Model/CardGameTable.swift
@@ -61,7 +61,7 @@ extension CardGameTable {
         for index in cardInfo.stackIndex...cardStacks[cardInfo.indexOfCard].count - 1 {
             cardPack.append(cardStacks[cardInfo.indexOfCard][index])
         }
-        return (dragToCardStacks(cardPack, cardInfo), touchedCard)
+        return (dragCard(cardPack, cardInfo), touchedCard)
     }
     
     func getCardInfo(_ card: Card) -> CardInfo {
@@ -82,8 +82,8 @@ extension CardGameTable {
     mutating private func moveCard(_ tappedCard: Card, _ tappedCardInfo: CardInfo) -> Bool {
         switch tappedCard {
         case _ where tappedCard.isAce() : return moveAce(tappedCard, tappedCardInfo)
-        case _ where tappedCard.isKing() : return moveKing(tappedCard, tappedCardInfo)
         case _ where moveToFoundation(tappedCard, tappedCardInfo) || moveToCardStacks(tappedCard, tappedCardInfo) : return true
+        case _ where tappedCard.isKing() : return moveKing(tappedCard, tappedCardInfo)
         default:
             return false
         }
@@ -102,7 +102,7 @@ extension CardGameTable {
     
     mutating private func moveToCardStacks(_ card: Card, _ cardInfo: CardInfo) -> Bool {
         for index in cardStacks.indices {
-            guard let lastCard = cardStacks[index].last else { return false }
+            guard let lastCard = cardStacks[index].last else { continue }
             guard card.isDifferentColor(lastCard) && lastCard.isNextRank(card) else { continue }
             cardStacks[index].append(card)
             guard cardInfo.position == .cardStacks else { return true }
@@ -130,6 +130,28 @@ extension CardGameTable {
         return false
     }
     
+}
+//Check Drag & Hand
+extension CardGameTable {
+    
+    mutating private func dragCard(_ cardPack : [Card], _ tappedCardInfo: CardInfo) -> Bool {
+        guard let topCard = cardPack.first else { return false }
+        if topCard.isKing() {
+            return dragKingCardPack(cardPack, tappedCardInfo)
+        }
+        return dragToCardStacks(cardPack, tappedCardInfo)
+    }
+    
+    mutating private func dragKingCardPack(_ cardPack: [Card], _ cardInfo: CardInfo) -> Bool {
+        for index in cardStacks.indices {
+            guard cardStacks[index].count == 0 else { continue }
+            cardStacks[index].append(contentsOf : cardPack)
+            cardStacks[cardInfo.indexOfCard].removeSubrange(cardInfo.stackIndex...cardStacks[cardInfo.indexOfCard].count - 1)
+            return true
+        }
+        return false
+    }
+    
     mutating private func dragToCardStacks(_ cardPack: [Card], _ cardInfo: CardInfo) -> Bool {
         for index in cardStacks.indices {
             guard let lastCard = cardStacks[index].last else { return false }
@@ -141,5 +163,5 @@ extension CardGameTable {
         }
         return false
     }
+    
 }
-
