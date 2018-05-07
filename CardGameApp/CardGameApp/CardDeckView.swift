5c91172050b92385ad632165d915b82be1cdae38
Mijin Jeon <mayihelpu27@gmail.com>
2018-05-07 16:26:13 +0900

    Feat: Add doubletap gesture to opened deck #10

--- CardGameApp/CardGameApp/CardDeckView.swift
+++ CardGameApp/CardGameApp/CardDeckView.swift
@@ -11,6 +11,7 @@ import UIKit
 class CardDeckView: UIView {
     var gameManager: CardGameManageable = CardGameDelegate.shared()
     var closedCardDeck = CardImageView()
+    var pickedCards = [ImageSelector]()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -66,10 +67,34 @@ class CardDeckView: UIView {
         let frameForDraw = CGRect(origin: newOrigin, size: ViewController.cardSize)
 
         let pickedCardView = CardImageView(frame: frameForDraw)
+        self.setDoubleTabToCard(to: pickedCardView)
+
         let pickedCard = gameManager.pickACard()
         pickedCard.turnOver()
-        pickedCardView.getImage(of: pickedCard)
+        self.pickedCards.append(pickedCard)
+
+        pickedCardView.getImage(of: self.pickedCards.last!)
         addSubview(pickedCardView)
     }
+
+    private func setDoubleTabToCard(to card: CardImageView) {
+        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(cardDoubleTapped(sender:)))
+        doubleTap.numberOfTapsRequired = 2
+        card.addGestureRecognizer(doubleTap)
+    }
+
+    @objc func cardDoubleTapped(sender: UITapGestureRecognizer) {
+        if sender.state == .ended {
+            let ddd = sender.view?.superview
+
+//            print((self.pickedCards.last! as! Card))
+            NotificationCenter.default.post(name: .doubleTappedOpenedDeck, object: self)
+        }
+    }
+
+    func removeOpenedCard() {
+
+    }
+
 
 }
