38a5ad31fbe01eead91e5377341bc66b6d63fcb5
Mijin Jeon <mayihelpu27@gmail.com>
2018-05-18 20:33:20 +0900

    Feat: Add MoveInfo class to handle info of view and model #31

--- CardGameApp/CardGameApp/FrameCalculator.swift
+++ CardGameApp/CardGameApp/FrameCalculator.swift
@@ -16,8 +16,9 @@ class FrameCalculator {
                                width: ViewController.widthOfRootView,
                                height: ViewController.heightOfRootView)
 
-    func originalLocation(view: UIView, position: CGPoint) -> (UIView, Int?, Int?) {
+    func originalLocation(view: UIView, position: CGPoint) -> (view: Movable, column: Int?, index: Int?) {
         if let from = view as? OneStack {
+            //guard let fromView = from as? Movable else { return (from, nil, nil) }
             guard let column = stackColumn(originX: from.frame.origin.x) else {
                 return (from, nil, nil)
             }
@@ -25,7 +26,7 @@ class FrameCalculator {
         } else if let from = view as? CardDeckView {
             return (from, nil, nil)
         }
-        return (view, nil, nil)
+        return (view as! Movable, nil, nil)
     }
 
     // origin.x로 검사하여 oneStackView의 column을 알아낸다.
@@ -40,6 +41,47 @@ class FrameCalculator {
     // origin.y를 검사하여 oneStack내에서의 cardIndex를 알아낸다.
     func cardIndexInStack(originY: CGFloat) -> Int {
         return Int(originY / 15.0)
+    }
+
+    func originalLocation2(view: UIView, position: CGPoint) -> MoveInfo {
+        if let from = view as? OneStack {
+            //guard let fromView = from as? Movable else { return (from, nil, nil) }
+            guard let column = stackColumn(originX: from.frame.origin.x) else {
+                return MoveInfo(view: from, column: nil, index: nil)
+            }
+            return MoveInfo(view: from, column: column, index: cardIndexInStack(originY: position.y))
+        } else if let from = view as? CardDeckView {
+            return MoveInfo(view: from, column: nil, index: nil)
+        }
+        return MoveInfo(view: view as! Movable, column: nil, index: nil)
+    }
+
+
+}
+
+class MoveInfo {
+    var view: Movable
+    var column: Int?
+    var index: Int?
+
+    init(view: Movable, column: Int?, index: Int?) {
+        self.view = view
+        self.column = column
+        self.index = index
+    }
+
+    func getView() -> Movable {
+        return self.view
+    }
+
+    func getColumn() -> Int? {
+        guard self.column != nil else { return nil }
+        return self.column!
+    }
+
+    func getIndex() -> Int? {
+        guard self.index != nil else { return nil }
+        return nil
     }
 
 }
