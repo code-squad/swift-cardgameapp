22d37c3953299ef04924489ed2f48079a15159bb
Clover <duwjdtmd91@gmail.com>
2018-06-16 14:50:34 +0900

    mainVC 생성: 카드 게임 생성되는 vc(foundationField: 카드를 모아두는 부분, deck field: 카드덱이 있는 부분, stackField: 카드 스텍들이 존재 할 부분)

---
+++ CardGameApp/CardGameApp/VC/MainVC.swift
@@ -0,0 +1,39 @@
+//
+//  MainVC.swift
+//  CardGameApp
+//
+//  Created by Jung seoung Yeo on 2018. 6. 14..
+//  Copyright © 2018년 Clover. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: BaseVC {
+    
+    private let foundationField = FoundationCardView()
+    private let deckField = DeckView()
+    private let stackField = StackCardView()
+    private var deck: Deck = Deck()
+    
+    override func setupView() {
+        super.setupView()
+        view.addSubView(foundationField, deckField, stackField)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        deck.shuffle()
+        stackField.makeInitStackView(makeStackCard())
+    }
+    
+    private func makeStackCard() -> [String] {
+        var stackCardNames: [String] = []
+        for _ in 1 ... 7 {
+            stackCardNames.append(deck.removeOne().description)
+        }
+        return stackCardNames
+    }
+    
+    
+}
