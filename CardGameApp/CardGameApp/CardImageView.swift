42d1f981cce8fa4935a6cc13cc2fde060ed13c15
Moon <myssun0325@gmail.com>
2018-07-17 17:48:27 +0900

    CardImageView 추가

---
+++ CardGameApp/CardGameApp/CardImageView.swift
@@ -0,0 +1,9 @@
+//
+//  CardImageView.swift
+//  CardGameApp
+//
+//  Created by moon on 2018. 7. 17..
+//  Copyright © 2018년 moon. All rights reserved.
+//
+
+import Foundation
--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -8,22 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct CardSize {
+    static let width = UIScreen.main.bounds.width / 7 * 0.9
+    static let height = CardSize.width * 1.27
+}
 
-    private struct Constants {
-        static let backgroundImageName = "bg_pattern"
-    }
+struct ImageName {
+    static let background = "bg_pattern"
+    static let cardBack = "card-back"
+}
+
+class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setBackGround()
+        setupBackGround()
+        
+        for order in 0..<7 {
+            let cardImageView = makeCardImageView()
+            view.addSubview(cardImageView)
+            cardImageView.frame.origin = CGPoint(x: self.view.frame.width / 7 * CGFloat(order),
+                                                 y: UIApplication.shared.statusBarFrame.height + 10)
+        }
     }
     
-    private func setBackGround() {
-        guard let image = UIImage(named: Constants.backgroundImageName) else { return }
-        self.view.backgroundColor = UIColor(patternImage: image)
+    private func setupBackGround() {
+        guard let backgroundImage = UIImage(named: ImageName.background) else { return }
+        self.view.backgroundColor = UIColor(patternImage: backgroundImage)
     }
-        
+    
+    private func makeCardImageView() -> UIImageView {
+        let cardImageView = UIImageView()
+        cardImageView.image = UIImage(named: ImageName.cardBack)
+        cardImageView.frame.size = CGSize(width: CardSize.width, height: CardSize.height)
+        return cardImageView
+    }
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
