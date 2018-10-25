56687500f44fe57242bc3131e637647763966b49
oingbong <oingbong@gmail.com>
2018-10-26 00:38:28 +0900

    Add PatternUIView(Custom UIView)

---
+++ CardGameApp/CardGameApp/PatternUIView.swift
@@ -0,0 +1,21 @@
+//
+//  PatternUIView.swift
+//  CardGameApp
+//
+//  Created by oingbong on 26/10/2018.
+//  Copyright © 2018 oingbong. All rights reserved.
+//
+
+import UIKit
+
+class PatternUIView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
