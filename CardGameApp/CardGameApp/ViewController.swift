df6c232b99f595694bac8047f867d53be3aec6f1
oingbong <oingbong@gmail.com>
2018-10-26 00:16:58 +0900

    Add Image & setting background Pattern

--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -12,7 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if let backgroundPattern = UIImage(named: "bg_pattern.png") {
+            self.view.backgroundColor = UIColor(patternImage: backgroundPattern)
+        }
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
