7cdad8ba74e0b739d5962e4e203601ba06f9fd54
oingbong <oingbong@gmail.com>
2018-10-26 17:21:51 +0900

    Add String Extension for add png

---
+++ CardGameApp/CardGameApp/String+PNG.swift
@@ -0,0 +1,15 @@
+//
+//  String+PNG.swift
+//  CardGameApp
+//
+//  Created by oingbong on 26/10/2018.
+//  Copyright © 2018 oingbong. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    var formatPNG: String {
+        return self + ".png"
+    }
+}
