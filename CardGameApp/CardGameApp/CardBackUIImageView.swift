7d2edccf25494b82a1b3c3aec2a4b81c4e92ee81
oingbong <oingbong@gmail.com>
2018-10-26 11:25:46 +0900

    Add CardBackUIImageView & display just one card

---
+++ CardGameApp/CardGameApp/CardBackUIImageView.swift
@@ -0,0 +1,31 @@
+//
+//  CardBackUIImageView.swift
+//  CardGameApp
+//
+//  Created by oingbong on 26/10/2018.
+//  Copyright © 2018 oingbong. All rights reserved.
+//
+
+import UIKit
+
+class CardBackUIImageView: UIImageView {
+    override init(image: UIImage?) {
+        super.init(image: image)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    public func reSize(with frame: CGRect) {
+        let viewWidth = frame.width
+        // 여유공간을 빼주기 위함 (즉, 전체화면의 10%를 더 빼고 카드넓이 계산)
+        let viewWidthWithSpace = viewWidth - viewWidth * 0.1
+        let imageWidth = viewWidthWithSpace / 7
+        let widthRatio = CGFloat(1)
+        let heightRatio = CGFloat(1.27)
+        // newSize
+        let newSize = CGSize(width: imageWidth * widthRatio, height: imageWidth * heightRatio)
+        self.frame.size = newSize
+    }
+}
--- CardGameApp/CardGameApp/PatternUIView.swift
+++ CardGameApp/CardGameApp/PatternUIView.swift
@@ -12,16 +12,25 @@ class PatternUIView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         defalutBackground()
+        listCards()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         defalutBackground()
+        listCards()
     }
     
     private func defalutBackground() {
         if let backgroundPattern = UIImage(named: "bg_pattern.png") {
             self.backgroundColor = UIColor(patternImage: backgroundPattern)
         }
+    }
+    
+    private func listCards() {
+        let image = UIImage(named: "card-back.png")
+        let cardBack = CardBackUIImageView(image: image)
+        cardBack.reSize(with: self.frame)
+        self.addSubview(cardBack)
     }
 }
