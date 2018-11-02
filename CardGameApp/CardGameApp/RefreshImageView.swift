47ffe6253c68a723f3e4e3ead7fb3cb298ae87ec
oingbong <oingbong@gmail.com>
2018-11-03 00:12:00 +0900

    Edit unused init in RefreshImageView

--- CardGameApp/CardGameApp/RefreshImageView.swift
+++ CardGameApp/CardGameApp/RefreshImageView.swift
@@ -12,11 +12,7 @@ class RefreshImageView: UIImageView {
     override init(image: UIImage?) {
         super.init(image: image)
     }
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-    }
-    
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
