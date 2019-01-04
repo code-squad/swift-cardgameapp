8c66e8f804cfcbbc83f3c3ca0d3455d7cdd9ec7c
DrakeYang <drake.the.programmer@gmail.com>
2019-01-04 16:49:45 +0900

    카드모양,번호 생성방법 변경 : switch > rawValue

--- CardGameApp/CardGameApp/model/GameBoard.swift
+++ CardGameApp/CardGameApp/model/GameBoard.swift
@@ -11,40 +11,13 @@ import Foundation
 struct GameBoard {
     /// 랜덤한 카드 한장을 리턴한다
     func makeRandomCard() -> Card? {
-        // 랜덤정수로 카드용 마크,숫자 생성
-        let randomMarkNumber = Int(arc4random_uniform(4)+1)
-        let randomNumberingNumber = Int(arc4random_uniform(13) + 1)
+        // 랜덤정수 생성
+        let randomMarkNumber = String(Int(arc4random_uniform(4)+1))
+        let randomNumberingNumber = String(Int(arc4random_uniform(13) + 1))
         
-        // 카드 마크 선언
-        var randomMark : Mark
-        // 랜덤정수를 마크에 입력
-        switch randomMarkNumber {
-        case 1 : randomMark = Mark.spade
-        case 2 : randomMark = .clover
-        case 3 : randomMark = .heart
-        case 4 : randomMark = .diamond
-        default : return nil
-        }
-        
-        // 카드 넘버링 선언
-        var randomNumbering : Numbering
-        // 랜덤정수를 넘버링에 입력
-        switch randomNumberingNumber {
-        case 1 : randomNumbering = Numbering.ace
-        case 2 : randomNumbering = .two
-        case 3 : randomNumbering = .three
-        case 4 : randomNumbering = .four
-        case 5 : randomNumbering = .five
-        case 6 : randomNumbering = .five
-        case 7 : randomNumbering = .seven
-        case 8 : randomNumbering = .eight
-        case 9 : randomNumbering = .nine
-        case 10 : randomNumbering = .ten
-        case 11 : randomNumbering = .jack
-        case 12 : randomNumbering = .queen
-        case 13 : randomNumbering = .king
-        default : return nil
-        }
+        // 카드용 마크,숫자 생성. 생성 실패리 닐 리턴
+        guard let randomMark = Mark(rawValue: randomMarkNumber),
+            let randomNumbering = Numbering(rawValue: randomNumberingNumber) else { return nil }
         
         // 카드 리턴
         return Card(mark: randomMark, numbering: randomNumbering)
