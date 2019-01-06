afc4c62905b539c8a27b691b3310519c948caa09
DrakeYang <drake.the.programmer@gmail.com>
2019-01-06 15:10:58 +0900

    CustomStringConvertible 프로토콜 준수부분 삭제

--- CardGameApp/CardGameApp/model/Slot.swift
+++ CardGameApp/CardGameApp/model/Slot.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class Slot : CustomStringConvertible{
+class Slot {
     // 카드배열 선언
     private var cardList : [Card]
     // 이니셜라이저
@@ -21,24 +21,6 @@ class Slot : CustomStringConvertible{
         return getInfo()
     }
     
-    /// 내용을 출력하는 함수
-    func getInfo() -> String{
-        // 카드가 없을경우 빈 배열모양을 리턴
-        if cardList.count == 0 {
-            return "[]"
-        }
-        // 리턴값 문자열로 변수 선언한다
-        var result : String = "["
-        // 반복문으로 결과값에 문자열을 쌓는다
-        for card in cardList {
-            result.append(card.name()+",")
-        }
-        // 마지막 , 를 지워준다
-        result.removeLast()
-        // 결과에 ] 붙여 리턴한다
-        return result+"]"
-    }
-    
     /// 카드 카운트 리턴
     func count() -> Int {
         return cardList.count
@@ -47,4 +29,6 @@ class Slot : CustomStringConvertible{
     func pop() -> Card? {
         return cardList.popLast()
     }
+    
+    
 }
