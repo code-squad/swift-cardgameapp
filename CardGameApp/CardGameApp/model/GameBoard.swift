58120fe4b8d45aa3fa31a348dfeaa2fd5ac0e908
DrakeYang <drake.the.programmer@gmail.com>
2019-01-07 20:19:31 +0900

    덱에서 오픈덱으로, 오픈덱에서 덱으로 카드이동 함수 추가

--- CardGameApp/CardGameApp/model/GameBoard.swift
+++ CardGameApp/CardGameApp/model/GameBoard.swift
@@ -12,7 +12,7 @@ class GameBoard {
     /// 덱 선언
     private var deck = Deck()
     /// 사용자가 오픈한 카드가 모이는 덱
-    private var openedDeck : Deck = Deck()
+    private var openedDeck : [Card] = []
     /// 점수를 얻는 칸
     private var pointCardSlot : [Card] = []
     /// 펼쳐놓는 카드들
@@ -70,49 +70,23 @@ class GameBoard {
         return playCard[lineNumber-1]
     }
     
-//    /// 슬롯 배열을 받아서 문자형 배열로 리턴
-//    private func getInfo(slots: [Slot]) -> [String] {
-//        // 결과 리턴용
-//        var result : [String] = []
-//
-//        // 정보에 몇번 유저 슬롯인지 정보를 입력해준다.
-//        for index in 1..<slots.count {
-//            result.append("참가자#\(index) "+slots[index].getInfo())
-//        }
-//        // 딜러 결과를 리턴한다
-//        result.append("딜러 "+slots[0].getInfo())
-//        // 결과를 리턴한다
-//        return result
-//    }
+    /// 덱을 오픈
+    func deckToOpened() -> CardInfo? {
+        // 덱에서 한장 추출. 없으면 닐 리턴
+        guard let popedCard = deck.removeOne() else { return nil }
+        // 추출한 카드를 열은덱 에 추가
+        openedDeck.append(popedCard)
+        // 추출한 카드의 정보를 리턴
+        return popedCard
+    }
     
-    //    /// 게임종류,인원수와 카드배열을 받아서 딜러,플레이어의 슬롯배열을 리턴
-    //    private mutating func makeSlots(gameMode: GameMode, playerNumber: Int) -> [Slot]? {
-    //        // slot 배열을 만든다. 인덱스 0 이 딜러, 이후 인덱스가 플레이어
-    //        var slotList : [Slot] = []
-    //        // 플레이어수 + 1 만큼 반복
-    //        for _ in 0...playerNumber {
-    //            // 게임 종류별로 필요한 만큼 카드를 뽑느다
-    //            guard let pickedCards = deck.removeCards(gameMode.rawValue) else {
-    //                // 카드가 다 떨어지면 게임을 종료한다
-    //                return nil
-    //            }
-    //            // 뽑은 카드를 슬롯 리스트에 넣는다
-    //            slotList.append(pickedCards)
-    //        }
-    //        return slotList
-    //    }
-    
-//    /// 게임모드,인원 을 받아서 게임결과를 문자형 배열로 리턴
-//    mutating func startCardGame(gameMode: GameMode, playerNumber: Int) -> [String]? {
-//        // 덱을 리셋하고 섞는다
-//        deck.reset()
-//        deck.shuffle()
-//        // 슬롯 배열을 만든다. 카드가 다 떨어지면 닐 리턴
-//        guard let slots = makeSlots(gameMode: gameMode, playerNumber: playerNumber) else {
-//            return nil
-//        }
-//        return getInfo(slots: slots)
-//    }
+    /// 오픈된 덱 전부를 다시 덱에 포함
+    func openedToDeck(){
+        // 오픈덱 전부를 덱에 추가한다
+        deck.addCards(cards: openedDeck)
+        // 오픈덱을 비운다
+        self.openedDeck = []
+    }
     
 }
 
