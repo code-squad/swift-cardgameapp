4a3bac35e8723df20a52a35a4e2f7037d07427f8
DrakeYang <drake.the.programmer@gmail.com>
2019-01-09 16:01:14 +0900

    에러메세지용 나열형 추가: errorMessage

---
+++ CardGameApp/CardGameApp/model/Error.swift
@@ -0,0 +1,13 @@
+//
+//  Error.swift
+//  CardGameApp
+//
+//  Created by Yoda Codd on 2019. 1. 9..
+//  Copyright © 2019년 Drake. All rights reserved.
+//
+
+import Foundation
+
+enum errorMessage : String, Error {
+    case wrongCardInfo = "잘못된 카드정보입니다."
+}
