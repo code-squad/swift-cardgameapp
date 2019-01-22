e12041452cfadaa594cfc9d6582a0bf3f9328279
DrakeYang <drake.the.programmer@gmail.com>
2019-01-22 17:03:17 +0900

    카드뷰가 카드정보객체가 아닌 이미지를 소지하도록 수정

--- CardGameApp/CardGameApp/CardView.swift
+++ CardGameApp/CardGameApp/CardView.swift
@@ -11,39 +11,42 @@ import os
 
 /// 카드 표현을 담당하는 이미지뷰
 class CardView : UIImageView {
-    /// 카드정보를 가진다
-    var cardInfo : CardInfo
+    // 카드 앞면 이미지
+    var cardFrontImage = UIImage()
+    // 뒷면 이미지는 공통
+    let cardBackImage = #imageLiteral(resourceName: "card-back")
+    // 카드가 앞면인지 뒷면인지
+    var isFront = true
     
     init(cardInfo: CardInfo, frame: CGRect){
-        self.cardInfo = cardInfo
+        let cardImage = UIImage(named: cardInfo.name()) ?? UIImage()
+        self.cardFrontImage = cardImage
+        // 카드이름과 표시화면이 같으면 앞면
+        self.isFront = cardInfo.image() == cardInfo.name()
         super.init(frame: frame)
-        self.image = UIImage(named: cardInfo.image())
+        // 앞뒷면 상태에 따라 이미지 표시
+        self.refreshImage()
     }
     
-    /// 저장기능은 아직 없음
+
+    /// required init
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
     }
     
     /// 카드뷰를 뒤집을 경우 뒤집은 후의 이미지로 교체한다
     func flip(){
-        // 카드정보에서 뒤집기
-        cardInfo.flip()
-        // 이미지를 추출
-        guard let flipedCardImage = UIImage(named: cardInfo.image()) else {
-            os_log("카드뷰 뒤집기 실패 : %@",cardInfo.name())
-            return ()
-            
-        }
-        // 추출된 이미지로 교체
-        self.image = flipedCardImage
+        self.isFront = !self.isFront
+        refreshImage()
     }
     
     /// 카드인포 내부에서 카드가 뒤집힐 경우를 위한 이미지 갱신
     func refreshImage(){
-        // 이미지 추출
-        let changedImage = UIImage(named: cardInfo.image())
-        // 이미지 갱신
-        self.image = changedImage
+        if isFront {
+            self.image = self.cardFrontImage
+        } else {
+            
+            self.image = self.cardBackImage
+        }
     }
 }
