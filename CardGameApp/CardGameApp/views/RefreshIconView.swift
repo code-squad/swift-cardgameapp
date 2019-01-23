d6e014b4ff7de38e7610b33689535fb83e62f8bd
DrakeYang <drake.the.programmer@gmail.com>
2019-01-23 17:06:59 +0900

    리프레시 아이콘 뷰 를 담당할 객체 파일로 분리

---
+++ CardGameApp/CardGameApp/views/RefreshIconView.swift
@@ -0,0 +1,21 @@
+//
+//  RefreshIconView.swift
+//  CardGameApp
+//
+//  Created by Yoda Codd on 2019. 1. 23..
+//  Copyright © 2019년 Drake. All rights reserved.
+//
+
+import UIKit
+
+class RefreshIconView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
