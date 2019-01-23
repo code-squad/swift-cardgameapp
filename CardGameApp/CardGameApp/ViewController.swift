9b48a772e1679b58cb11d218dd6cb82bfc069dbc
popsmile <popsmile7@gmail.com>
2019-01-23 18:26:49 +0900

    Add CardImageViews to the ViewController with a struct CardImageViewCreater

--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setBackground()
+        addCardImageViews()
     }
 
     private func setBackground() {
@@ -24,5 +25,38 @@ class ViewController: UIViewController {
         return .lightContent
     }
 
+    private func addCardImageViews() {
+        let cardCreater = CardImageViewCreater(numberOfCards: 7, sideMargin: 5, topMargin: 40)
+        let cards = cardCreater.createHorizontally(within: self.view.frame.width)
+        cards.forEach { self.view.addSubview($0) }
+    }
+
 }
 
+extension ViewController {
+
+    private struct CardImageViewCreater {
+        let numberOfCards: Int
+        let sideMargin: CGFloat
+        let topMargin: CGFloat
+
+        private func calculateCardWidth(with frameWidth: CGFloat) -> CGFloat {
+            return (frameWidth - sideMargin * CGFloat(numberOfCards + 2)) / CGFloat(numberOfCards)
+        }
+
+        func createHorizontally(within frameWidth: CGFloat) -> [CardImageView] {
+            var cards: [CardImageView] = []
+            let width = calculateCardWidth(with: frameWidth)
+            var positionX = sideMargin * 1.5
+            for _ in 1...numberOfCards {
+                let origin = CGPoint(x: positionX, y: topMargin)
+                let card = CardImageView(origin: origin, width: width)
+                cards.append(card)
+                positionX += width + sideMargin
+            }
+            return cards
+        }
+
+    }
+
+}
