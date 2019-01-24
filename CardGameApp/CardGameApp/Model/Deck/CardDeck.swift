ba0012203d3799e53192c2ef1edce7ffec02efaa
popsmile <popsmile7@gmail.com>
2019-01-24 11:58:48 +0900

    Classify the files into more specific groups

--- CardGameApp/CardGameApp/Model/Deck/CardDeck.swift
+++ CardGameApp/CardGameApp/Model/Deck/CardDeck.swift
@@ -58,22 +58,4 @@ struct CardDeck: Playable {
         shuffle()
     }
 
-    mutating func makeResult(of menu: MenuItem) -> String {
-        switch menu {
-        case .reset:
-            reset()
-            return "카드 전체를 초기화했습니다."
-        case .shuffle:
-            shuffle()
-            return "전체 \(self.count())장의 카드를 섞었습니다."
-        case .draw:
-            if let card = removeOne() {
-                return "\(card)\n총 \(self.count())장의 카드가 남아있습니다."
-            }
-            return "남아있는 카드가 없습니다."
-        case .quit:
-            return "---- END ----"
-        }
-    }
-
 }
