9b4c0001cc352ad11221603c3263bdb46fe1cd8a
DrakeYang <drake.the.programmer@gmail.com>
2019-01-25 11:22:10 +0900

    포인트덱관리자의 포인트덱리스트,생성자,체크함수 추가

--- CardGameApp/CardGameApp/model/PointDeckManager.swift
+++ CardGameApp/CardGameApp/model/PointDeckManager.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+
+/// 포인트덱을 관리하는 객체
+class PointDeckManager {
+    //// 포인트덱 4개 소지
+    private var pointDeckList : [PointDeck]
+    
+    /// 생성자
+    init(){
+        pointDeckList = []
+        
+        // 모든 마크 생성
+        let allMarks = Mark.allCases()
+        
+        // 마크별로 추가
+        for mark in allMarks {
+            pointDeckList.append(PointDeck(mark: mark))
+        }
+    }
+    
+    /// 카드정보를 받아서 추가 가능한지 체크
+    func checkAdd(cardInfo: CardInfo) -> Bool {
+        // 모든 덱을 체크
+        for pointDeck in pointDeckList {
+            // 추가가능 하다면 참
+            if pointDeck.checkAdd(cardInfo: cardInfo) {
+                return true
+            }
+        }
+        
+        // 추가가능한 덱이 없다면 거짓
+        return false
+    }
+    
+    /// 카드객체를 알맞는 곳에 추가
+    
+    
+    
+}
