9fcb9bd5c2ae8131c44fd6a6a765d3eba7c57f5d
popsmile <popsmile7@gmail.com>
2019-01-25 14:16:16 +0900

    Create a custom UIView named CardSpaceView

---
+++ CardGameApp/CardGameApp/View/CardSpaceView.swift
@@ -0,0 +1,44 @@
+//
+//  CardSpaceView.swift
+//  CardGameApp
+//
+//  Created by 윤지영 on 25/01/2019.
+//  Copyright © 2019 윤지영. All rights reserved.
+//
+
+import UIKit
+
+class CardSpaceView: UIView {
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setUp()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUp()
+    }
+    
+    convenience init(origin: CGPoint, width: CGFloat) {
+        let size = CGSize(width: width, height: width * 1.27)
+        let frame = CGRect(origin: origin, size: size)
+        self.init(frame: frame)
+    }
+    
+    private func setUp() {
+        colorBorder()
+        roundCorners()
+    }
+    
+    private func colorBorder() {
+        layer.borderColor = UIColor.white.cgColor
+        layer.borderWidth = 0.5
+    }
+    
+    private func roundCorners() {
+        layer.cornerRadius = 4
+        layer.masksToBounds = true
+    }
+    
+}
