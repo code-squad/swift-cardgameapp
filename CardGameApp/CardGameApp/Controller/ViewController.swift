71d5a994fb462e8e7f38dcabc238fc18a23b10ae
popsmile <popsmile7@gmail.com>
2019-01-25 11:36:27 +0900

    Add methods to draw CardImageViews in a different way

--- CardGameApp/CardGameApp/Controller/ViewController.swift
+++ CardGameApp/CardGameApp/Controller/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     private var cardImages: [CardImageView]!
+    private var cardDeckImage: CardImageView!
     private var cardDeck: CardDeck
 
     required init?(coder aDecoder: NSCoder) {
@@ -21,6 +22,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         setBackground()
         addCardImageViews()
+        addEmptyCardImageViews()
+        setCardDeckImage()
     }
 
     private func setBackground() {
@@ -32,10 +35,24 @@ class ViewController: UIViewController {
         return .lightContent
     }
 
+    private func setCardDeckImage() {
+        guard let cards = cardDeck.removeMultiple(by: 1) else { return }
+        let cardImageViewCreater = CardImageViewCreater(cards: cards, sideMargin: 5, topMargin: 20)
+        let cardImageViews = cardImageViewCreater.createFewHorizontally(within: self.view.frame.width, divided: 7, align: .right)
+        cardImageViews.forEach { self.view.addSubview($0) }
+    }
+
+    private func addEmptyCardImageViews() {
+        guard let cards = cardDeck.removeMultiple(by: 4) else { return }
+        let cardImageViewCreater = CardImageViewCreater(cards: cards, sideMargin: 5, topMargin: 20)
+        let cardImageViews = cardImageViewCreater.createFewHorizontally(within: self.view.frame.width, divided: 7, align: .left)
+        cardImageViews.forEach { self.view.addSubview($0) }
+    }
+
     private func addCardImageViews() {
         guard let cards = cardDeck.removeMultiple(by: 7) else { return }
         let cardImageViewCreater = CardImageViewCreater(cards: cards, sideMargin: 5, topMargin: 100)
-        let cardImageViews = cardImageViewCreater.createHorizontally(within: self.view.frame.width)
+        let cardImageViews = cardImageViewCreater.createFullHorizontally(within: self.view.frame.width)
         cardImageViews.forEach { self.view.addSubview($0) }
         cardImages = cardImageViews
     }
@@ -58,12 +75,15 @@ extension ViewController {
         let sideMargin: CGFloat
         let topMargin: CGFloat
 
-        private func calculateCardWidth(with frameWidth: CGFloat) -> CGFloat {
-            let numberOfCards = cards.count
-            return (frameWidth - sideMargin * CGFloat(numberOfCards + 2)) / CGFloat(numberOfCards)
+        private func calculateCardWidth(with frameWidth: CGFloat, divided: Int? = nil) -> CGFloat {
+            let calculation = { number in (frameWidth - self.sideMargin * CGFloat(number + 2)) / CGFloat(number) }
+            if let number = divided {
+                return calculation(number)
+            }
+            return calculation(cards.count)
         }
 
-        func createHorizontally(within frameWidth: CGFloat) -> [CardImageView] {
+        func createFullHorizontally(within frameWidth: CGFloat) -> [CardImageView] {
             var cardImageViews: [CardImageView] = []
             let width = calculateCardWidth(with: frameWidth)
             var positionX = sideMargin * 1.5
@@ -73,6 +93,29 @@ extension ViewController {
                 cardImageView.setImage(named: imageName)
                 cardImageViews.append(cardImageView)
                 positionX += width + sideMargin
+            }
+            return cardImageViews
+        }
+
+        enum Align: CGFloat {
+            case left = 1
+            case right = -1
+        }
+
+        func createFewHorizontally(within frameWidth: CGFloat, divided: Int, align: Align) -> [CardImageView] {
+            var cardImageViews: [CardImageView] = []
+            let width = calculateCardWidth(with: frameWidth, divided: divided)
+            var positionX = sideMargin * 1.5
+            if align == .right {
+                positionX = frameWidth - width - positionX
+            }
+            let direction = align.rawValue
+            for imageName in cards.imageNames {
+                let origin = CGPoint(x: positionX, y: topMargin)
+                let cardImageView = CardImageView(origin: origin, width: width)
+                cardImageView.setImage(named: imageName)
+                cardImageViews.append(cardImageView)
+                positionX += (width + sideMargin) * direction
             }
             return cardImageViews
         }
