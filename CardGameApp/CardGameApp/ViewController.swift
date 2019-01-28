073a00f63283f7a673e9b9d38761f93c389abeae
hngfu <djsl1018@naver.com>
2019-01-28 15:58:08 +0900

    ViewController에서 카드 7장 추가 하도록 핢

--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -9,15 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         guard let image = UIImage(named: "bg_pattern") else { return }
         self.view.backgroundColor = UIColor(patternImage: image)
+        
+        let numberOfCards = 7
+        let bounds = UIScreen.main.bounds
+        let ratio = 1.27
+        let space = 4
+        let totalSpace = (numberOfCards + 1) * space
+        let cardWidth = (bounds.width - CGFloat(totalSpace)) / CGFloat(numberOfCards)
+        let cardHeight = cardWidth * CGFloat(ratio)
+        var cardPointX: CGFloat = CGFloat(space)
+        
+        for _ in 0..<numberOfCards {
+            let imageView = UIImageView()
+            imageView.image = UIImage(named: "card_back")
+            imageView.frame = .init(x: cardPointX, y: 40, width: cardWidth, height: cardHeight)
+            cardPointX += cardWidth + CGFloat(space)
+            self.view.addSubview(imageView)
+        }
     }
-
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
