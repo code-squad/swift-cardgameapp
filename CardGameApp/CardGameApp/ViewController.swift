166179beb7fb0c72701ff4a19701e51b3db076d7
hngfu <djsl1018@naver.com>
2019-01-28 13:50:11 +0900

    view background를 bg_pattern으로 꾸며줌

--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        guard let image = UIImage(named: "bg_pattern") else { return }
+        self.view.backgroundColor = UIColor(patternImage: image)
     }
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
