891873b43d9dd5f2af1ae48ef67174218c188f0c
hngfu <djsl1018@naver.com>
2019-01-29 00:41:46 +0900

    makeCardBackImageViews()와 Int,Double에 extension했던 cgFloat() 삭제

--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -25,46 +25,5 @@ class ViewController: UIViewController {
         
         guard let image = UIImage(named: "bg_pattern") else { return }
         self.view.backgroundColor = UIColor(patternImage: image)
-        
-        makeCardBackImageViews()
-    }
-    
-    //MARK: Private
-    
-    private func makeCardBackImageViews() {
-        
-        let numberOfCards = 7
-        let bounds = UIScreen.main.bounds
-        let ratio = 1.27
-        let space = 4
-        let totalSpace = (numberOfCards + 1) * space
-        let cardWidth: CGFloat = (bounds.width - totalSpace.cgFloat()) / numberOfCards.cgFloat()
-        let cardHeight: CGFloat = cardWidth * ratio.cgFloat()
-        var cardPointX: CGFloat = space.cgFloat()
-        let cardPointY: CGFloat = 40
-        
-        for _ in 0..<numberOfCards {
-            let cardBackFrame = CGRect(x: cardPointX,
-                                       y: cardPointY,
-                                       width: cardWidth,
-                                       height: cardHeight)
-            let cardBackImage = UIImage(named: "card_back")
-            let imageView = UIImageView(image: cardBackImage)
-            imageView.frame = cardBackFrame
-            cardPointX += cardWidth + space.cgFloat()
-            self.view.addSubview(imageView)
-        }
-    }
-}
-
-extension Int {
-    func cgFloat() -> CGFloat {
-        return CGFloat(self)
-    }
-}
-
-extension Double {
-    func cgFloat() -> CGFloat {
-        return CGFloat(self)
     }
 }
