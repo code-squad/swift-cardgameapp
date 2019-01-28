833a3d5a3c24a4fd7b8da47c11ac4dbebdac738d
hngfu <djsl1018@naver.com>
2019-01-28 16:16:28 +0900

    CardBackImageView 생성

---
+++ CardGameApp/CardGameApp/CardBackImageView.swift
@@ -0,0 +1,21 @@
+//
+//  CardBackImageView.swift
+//  CardGameApp
+//
+//  Created by 조재흥 on 19. 1. 28..
+//  Copyright © 2019 hngfu. All rights reserved.
+//
+
+import UIKit
+
+class CardBackImageView: UIImageView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+}
