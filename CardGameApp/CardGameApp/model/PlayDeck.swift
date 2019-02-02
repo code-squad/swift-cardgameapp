d67713a09bb2f61fade4aa8fec1f3d69b696de15
DrakeYang <drake.the.programmer@gmail.com>
2019-02-02 23:29:47 +0900

    플레이덱에 카드추가, 추가가능카드 체크함수 추가

--- CardGameApp/CardGameApp/model/PlayDeck.swift
+++ CardGameApp/CardGameApp/model/PlayDeck.swift
@@ -17,22 +17,20 @@ class PlayDeck {
     /// 카드를 받아서 추가 가능한 카드인지 체크
     func checkAdd(card: Card) -> Bool {
         // 카드리스트에 가드가 존재하는지 체크
-        guard let lastCard = cardList.popLast() else {
+        guard let lastCard = cardList.last else {
             // 한장도 없을경우 K = 마지막넘버링 만 추가가능하다.
             return card.getNumberingRank() == Numbering.allCases().count - 1
         }
         // 카드리스트에 카드가 존재. 색이 다른지 체크
         if checkDifferentMarkColor(cardOne: card, cardTwo: lastCard) {
-            // 색이 같으면 이전 넘버링인지 체크
+            // 색이 같음, 이전 넘버링인지 체크
             if lastCard.markNumberingDifference(cardInfo: card) == -1 {
                 return true
             }
         }
         
-        // 색이 같거나 이전 넘버링이 아니면 안됨. 뽑은 카드 다시 리턴
-        cardList.append(lastCard)
+        // 색이 같거나 이전 넘버링이 아니면 안됨
         return false
-        
     }
     
     
@@ -40,10 +38,20 @@ class PlayDeck {
     
     
     
-    // 다른색인지 구분하는 함수. 플레이카드는 다른색,넘버링+1 만 들어올수 있다
+    /// 다른색인지 구분하는 함수. 플레이카드는 다른색,넘버링+1 만 들어올수 있다
     func checkDifferentMarkColor(cardOne: Card, cardTwo: Card) -> Bool {
         // 스페이드,클로버는 짝수. 하트,다이아는 홀수다. 2로 나눈 값이 달라야 추가 가능하다
         return cardOne.getMarkRank() % 2 != cardTwo.getMarkRank() % 2
         
     }
+    
+    /// 카드를 받아서 추가함. 내부에서 체크함수 돌림
+    func addCard(card: Card) {
+        if checkAdd(card: card) {
+            cardList.append(card)
+        }
+    }
+    
+    
+    
 }
