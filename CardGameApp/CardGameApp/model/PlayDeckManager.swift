911cb9be0f77964e5b822759a06f1d180acbdaf8
DrakeYang <drake.the.programmer@gmail.com>
2019-02-03 15:19:17 +0900

    플레이덱 매니저 객체추가

---
+++ CardGameApp/CardGameApp/model/PlayDeckManager.swift
@@ -0,0 +1,14 @@
+//
+//  PlayDeckManager.swift
+//  CardGameApp
+//
+//  Created by Yoda Codd on 2019. 2. 3..
+//  Copyright © 2019년 Drake. All rights reserved.
+//
+
+import Foundation
+
+class PlayDeckManager {
+    
+    
+}
