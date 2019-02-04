45183c8c8e7d21406de28ba6d331c94bcda3e340
popsmile <popsmile7@gmail.com>
2019-02-04 20:34:25 +0900

    Create a CardPileView to include a CardPileViewModel

---
+++ CardGameApp/CardGameApp/View/CardPileView.swift
@@ -0,0 +1,27 @@
+//
+//  CardPileView.swift
+//  CardGameApp
+//
+//  Created by 윤지영 on 04/02/2019.
+//  Copyright © 2019 윤지영. All rights reserved.
+//
+
+import UIKit
+
+class CardPileView: UIView {
+    private var viewModel: CardPileViewModel!
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    convenience init(frame: CGRect, viewModel: CardPileViewModel) {
+        self.init(frame: frame)
+        self.viewModel = viewModel
+    }
+
+}
