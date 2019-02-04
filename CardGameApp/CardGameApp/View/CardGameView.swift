0e9435f5ceb7956898e87cb0230d83337864e4d8
popsmile <popsmile7@gmail.com>
2019-02-04 18:05:26 +0900

    Create a CardGameView to include all the subviews

---
+++ CardGameApp/CardGameApp/View/CardGameView.swift
@@ -0,0 +1,21 @@
+//
+//  CardGameView.swift
+//  CardGameApp
+//
+//  Created by 윤지영 on 04/02/2019.
+//  Copyright © 2019 윤지영. All rights reserved.
+//
+
+import UIKit
+
+class CardGameView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
