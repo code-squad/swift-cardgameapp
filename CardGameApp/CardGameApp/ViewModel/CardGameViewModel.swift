7ac4b63fe36a8268f115cf665cda2a01ce14f644
popsmile <popsmile7@gmail.com>
2019-02-04 20:46:44 +0900

    Create a CardGameViewModel

---
+++ CardGameApp/CardGameApp/ViewModel/CardGameViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CardGameViewModel.swift
+//  CardGameApp
+//
+//  Created by 윤지영 on 04/02/2019.
+//  Copyright © 2019 윤지영. All rights reserved.
+//
+
+import Foundation
+
+class CardGameViewModel {
+    
+}
