dfd130899e9a887e95216fa7ee4d3dd4e299956c
DrakeYang <drake.the.programmer@gmail.com>
2019-02-14 15:38:22 +0900

    카드를 받아서 추가하는 함수 추가

--- CardGameApp/CardGameApp/model/PlayDeckManager.swift
+++ CardGameApp/CardGameApp/model/PlayDeckManager.swift
@@ -9,6 +9,16 @@
 import Foundation
 
 class PlayDeckManager {
+    /// 플레이덱 배열화
+    var playDeckList : [PlayDeck] = []
     
+    /// 카드를 받아서 추가
+    func addCard(card: Card) {
+        // 모든 덱 대상
+        for playDeck in playDeckList {
+            // 추가가능여부 체크는 플레이덱에서 수행됨
+            playDeck.addCard(card: card)
+        }
+    }
     
 }
