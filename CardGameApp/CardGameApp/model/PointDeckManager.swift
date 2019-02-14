3a74bcd91f06c3cb149f165553f8448341a44f2d
DrakeYang <drake.the.programmer@gmail.com>
2019-02-14 16:01:00 +0900

    포인트덱 카드추가함수 변형내용 매니저에 적용 : 리턴값 카드정보를 받아서 리턴

--- CardGameApp/CardGameApp/model/PointDeckManager.swift
+++ CardGameApp/CardGameApp/model/PointDeckManager.swift
@@ -26,42 +26,16 @@ class PointDeckManager {
         }
     }
     
-    /// 카드객체를 알맞는 곳에 추가
-    func addCard(card: Card) {
-        // 추가 가능한 곳이 있어야 한다
-        guard let checkedMark = checkAdd(card: card) else {
-            // 추가 불가능
-            return ()
-        }
-        
-        // 확인된 마크로 카드를 추가해준다
-        addCard(card: card, mark: checkedMark)
-    }
-    
-    /// 카드정보를 받아서 추가 가능한지 체크
-    private func checkAdd(card: Card) -> Mark? {
-        // 모든 덱을 체크
-        for pointDeck in pointDeckList {
-            // 추가가능 하다면 해당 마크 리턴
-            if pointDeck.checkAdd(card: card) {
-                return pointDeck.mark
-            }
-        }
-        
-        // 추가가능한 덱이 없다면 닐 리턴
-        return nil
-    }
-    
     /// 카드객체와 마크를 받아서 맞는 포인트덱에 카드 추가
-    private func addCard(card: Card, mark: Mark) {
+    func addCard(card: Card, mark: Mark) -> CardInfo? {
         // 모든 포인트덱이 대상
         for pointDeck in pointDeckList {
-            // 마크가 일치하면
-            if pointDeck.mark == mark {
-                // 추가 후 바로 리턴
-                pointDeck.addCard(card: card)
-                return ()
+            // 추가성공시 추가된 카드 정보를 리턴
+            if let addedCard = pointDeck.addCard(card: card) {
+                return addedCard
             }
         }
+        // 추가 실패시 닐리턴
+        return nil
     }
 }
