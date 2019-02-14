a28111a5fc2e39cdda0032aab93e7e22eaea0e26
DrakeYang <drake.the.programmer@gmail.com>
2019-02-14 16:04:37 +0900

    플레이덱 카드추가 함수 리턴값 변경을 플레이덱매니저에 적용

--- CardGameApp/CardGameApp/model/PlayDeckManager.swift
+++ CardGameApp/CardGameApp/model/PlayDeckManager.swift
@@ -13,12 +13,16 @@ class PlayDeckManager {
     var playDeckList : [PlayDeck] = []
     
     /// 카드를 받아서 추가
-    func addCard(card: Card) {
+    func addCard(card: Card) -> CardInfo? {
         // 모든 덱 대상
         for playDeck in playDeckList {
-            // 추가가능여부 체크는 플레이덱에서 수행됨
-            playDeck.addCard(card: card)
+            // 추가성공시 추가된 카드 정보를 리턴
+            if let addedCard = playDeck.addCard(card: card) {
+                return addedCard
+            }
         }
+        // 추가 실패리 닐리턴
+        return nil
     }
     
 }
