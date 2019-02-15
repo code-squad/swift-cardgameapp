df55a542c85008998a7ce9973e5337e631d2430b
DrakeYang <drake.the.programmer@gmail.com>
2019-02-15 16:46:33 +0900

    오픈덱에 함수추가 : 카드추가,리셋,전체리턴,카운트

--- CardGameApp/CardGameApp/model/OpendedDeck.swift
+++ CardGameApp/CardGameApp/model/OpendedDeck.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+/// 덱에서 오픈된 카드들이 모이는곳
+class OpenedDeck {
+    var cardList : [Card] = []
+    
+    
+    /// 리셋
+    func reset(){
+        self.cardList = []
+    }
+    
+    /// 카드추가
+    func addCard(card: Card){
+        self.cardList.append(card)
+    }
+    
+    /// 카드 전체 리턴
+    func pickAllCard() -> [Card] {
+        // 카드 전체를 미리 담아놓는다
+        let allCard = self.cardList
+        // 기존 리스트는 초기화
+        self.cardList = []
+        // 전체값 리턴
+        return allCard
+    }
+    
+    /// 카드 카운트
+    func count() -> Int {
+        return self.cardList.count
+    }
+}
