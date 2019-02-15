00cff967c3214a9497c80c800afdcdc59fc87d55
Dongmin.yoon <ehdals8965@naver.com>
2019-02-15 15:21:32 +0900

    Set Background Image from default to Pattern Image

--- CardGameApp/CardGameApp/ViewController.swift
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var cards: [UIImageView]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        initialBackground()
+        initialCards()
+    }
+    
+    private func initialBackground() {
+        self.view.backgroundColor = UIColor.init(patternImage: UIImage(named: "bg_pattern")!)
+    }
+    
+    private func initialCards() {
+        for card in cards { card.image = UIImage(named: "card-back") }
     }
 }
 
