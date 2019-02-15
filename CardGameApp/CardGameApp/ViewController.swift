c14e53ff7b14655fb5faedae9ee3d0eb3596b454
Dongmin.yoon <ehdals8965@naver.com>
2019-02-15 14:48:52 +0900

    Change UIStatusBarStyle from Default to LightContents

---
+++ CardGameApp/CardGameApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CardGameApp
+//
+//  Created by 윤동민 on 15/02/2019.
+//  Copyright © 2019 윤동민. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
