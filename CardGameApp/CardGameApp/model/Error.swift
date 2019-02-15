e4108b29c5b12bcbe75275eaf30a48a87a187cd5
DrakeYang <drake.the.programmer@gmail.com>
2019-02-15 15:15:27 +0900

    플레이덱을 만들 카드가 덱에 부족하다는 에러 추가

--- CardGameApp/CardGameApp/model/Error.swift
+++ CardGameApp/CardGameApp/model/Error.swift
@@ -11,4 +11,5 @@ import Foundation
 enum ErrorMessage : String, Error {
     case wrongCardInfo = "잘못된 카드정보입니다."
     case playDeckSettingFail = "플레이덱 세팅 실패"
+    case notEnoughCardForPlayDeck = "플레이덱을 만들 카드가 부족합니다."
 }
