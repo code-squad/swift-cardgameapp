389f1fe9272493bfb8b64eb04986ee337efcd42c
DrakeYang <drake.the.programmer@gmail.com>
2019-02-18 22:07:11 +0900

    오픈덱뷰 선언

---
+++ CardGameApp/CardGameApp/views/OpenedDeckView.swift
@@ -0,0 +1,21 @@
+//
+//  OpenedDeckView.swift
+//  CardGameApp
+//
+//  Created by Yoda Codd on 2019. 2. 18..
+//  Copyright © 2019년 Drake. All rights reserved.
+//
+
+import UIKit
+
+class OpenedDeckView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
