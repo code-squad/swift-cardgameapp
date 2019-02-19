850eaace2efb8673c21033b973b311e43bb3dfb0
DrakeYang <drake.the.programmer@gmail.com>
2019-02-19 17:47:34 +0900

    포인트덱뷰 위치조정, 뷰추가 함수 추가

--- CardGameApp/CardGameApp/views/PointDeckView.swift
+++ CardGameApp/CardGameApp/views/PointDeckView.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import os
 
 class PointDeckView: UIStackView {
 
@@ -18,4 +19,49 @@ class PointDeckView: UIStackView {
     required init(coder: NSCoder) {
         super.init(coder: coder)
     }
+    
+    /// 위치설정. 카드사이즈를 통째로 불러와서 설정한다
+    func setPointDeckView(origin: CGPoint, cardSize: CardSize){
+        // 뷰 오리진 수정
+        self.frame.origin = origin
+        
+        // 뷰 사이즈 수정
+        // 세로는 다른 뷰들과 같다
+        self.frame.size.height = cardSize.height
+        // 가로는 카드 4장 길이 필요
+        self.frame.size.width = cardSize.originWidth * 4 - cardSize.widthPadding * 2
+        
+        self.axis = .horizontal
+        
+        self.spacing = cardSize.widthPadding * 2
+        
+        self.distribution = .fillEqually
+        
+        setSubView(cardSize: cardSize)
+    }
+    
+    /// 빈 카드뷰 추가
+    private func setSubView(cardSize: CardSize){
+        for _ in Mark.allCases() {
+            // 카드 기준점 설정
+            let viewPoint = CGPoint(x: 0, y: 0)
+            // 기준점에서 카드사이즈로 이미지뷰 생성
+            let emptyCardView = EmptyPointCardView(origin: viewPoint, size: cardSize.cardSize)
+            
+            self.addArrangedSubview(emptyCardView)
+        }
+    }
+    
+    /// 카드뷰를 받아서 알맞은 위치에 추가한다
+    func addPointCardView(view: UIView){
+        // 카드뷰 변환 가능한지 체크
+        guard let cardView = view as? CardView else {
+            os_log("포인트뷰덱에 추가 실패")
+            return ()
+        }
+        // 라인에 따라 추가위치 변경
+        let superView = self.subviews[cardView.cardInfo.getDeckLine()]
+        superView.addSubview(cardView)
+    }
 }
+
