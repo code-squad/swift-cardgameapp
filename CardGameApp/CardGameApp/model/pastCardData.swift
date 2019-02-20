9640b53c5a5cac59bb45606edfb0efc8161fa696
DrakeYang <drake.the.programmer@gmail.com>
2019-02-20 20:22:16 +0900

    카드 이동전 카드정보를 담당하는 객체 생성 : 과거카드데이터

---
+++ CardGameApp/CardGameApp/model/pastCardData.swift
@@ -0,0 +1,20 @@
+//
+//  pastCardData.swift
+//  CardGameApp
+//
+//  Created by Yoda Codd on 2019. 2. 20..
+//  Copyright © 2019년 Drake. All rights reserved.
+//
+
+import Foundation
+
+/// 카드 이동전의 데이터가 모이는 객체
+class Past {
+    let deckType : DeckType
+    let deckLine : Int
+    
+    init(cardInfo: CardInfo){
+        self.deckType = cardInfo.getDeckType()
+        self.deckLine = cardInfo.getDeckLine()
+    }
+}
