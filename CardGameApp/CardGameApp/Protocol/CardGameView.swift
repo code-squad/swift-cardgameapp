04da0b27d84467d37d97597714b11e4724929495
hngfu <djsl1018@naver.com>
2019-02-20 18:49:15 +0900

    [CardGameView] 추가

---
+++ CardGameApp/CardGameApp/Protocol/CardGameView.swift
@@ -0,0 +1,13 @@
+//
+//  CardGameView.swift
+//  CardGameApp
+//
+//  Created by 조재흥 on 19. 2. 20..
+//  Copyright © 2019 hngfu. All rights reserved.
+//
+
+import Foundation
+
+protocol CardGameView {
+    func update(cards: [Card])
+}
