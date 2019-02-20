795bfaf5c8eae55c0de7da00ea2a8a86212474cb
DrakeYang <drake.the.programmer@gmail.com>
2019-02-20 18:53:46 +0900

    카드 원복 실패 에러 추가

--- CardGameApp/CardGameApp/model/Error.swift
+++ CardGameApp/CardGameApp/model/Error.swift
@@ -12,4 +12,5 @@ enum ErrorMessage : String, Error {
     case wrongCardInfo = "잘못된 카드정보입니다."
     case playDeckSettingFail = "플레이덱 세팅 실패"
     case notEnoughCardForPlayDeck = "플레이덱을 만들 카드가 부족합니다."
+    case failedUndoCard = "뽑은 카드를 제자리에 놓지 못했습니다."
 }
