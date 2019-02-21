795edc5d7e8b0f533d6d8cb92ac9d5ff68386cee
DrakeYang <drake.the.programmer@gmail.com>
2019-02-21 11:58:42 +0900

    플레이덱뷰 분리 적용 : 플레이덱뷰 생성함수 삭제

--- CardGameApp/CardGameApp/viewModel/PlayDeckViewManager.swift
+++ CardGameApp/CardGameApp/viewModel/PlayDeckViewManager.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+import UIKit
+
+/// 플레이덱뷰 를 가지는 객체
+class PlayDeckViewManager {
+    var stackViewList : [UIStackView] = []
+    
+    /// 카드사이즈와 좌표를 받아서 스택뷰 구성
+    func makeDeckViews(cardSize: CardSize, xPositions: [CGFloat], yPositions: [CGFloat]){
+        for count in 0..<cardSize.maxCardCount {
+            // 스택뷰 구성
+            let stackView = PlayDeckView(cardSize: cardSize, x: xPositions[count], y: yPositions[count])
+            self.stackViewList.append(stackView)
+        }
+    }
+}
