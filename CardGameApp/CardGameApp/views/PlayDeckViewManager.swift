ccdbb57d36a243bfab80591c4b728316fc703982
DrakeYang <drake.the.programmer@gmail.com>
2019-02-21 12:18:55 +0900

    플레이덱뷰매니저 뷰로 변경

---
+++ CardGameApp/CardGameApp/views/PlayDeckViewManager.swift
@@ -0,0 +1,21 @@
+//
+//  PlayDeckViewManager.swift
+//  CardGameApp
+//
+//  Created by Yoda Codd on 2019. 2. 21..
+//  Copyright © 2019년 Drake. All rights reserved.
+//
+
+import UIKit
+
+class PlayDeckViewManager: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
