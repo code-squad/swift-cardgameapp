eb9f7019ea8a0940f1089925ab6e64de83361f11
DrakeYang <drake.the.programmer@gmail.com>
2019-02-22 15:58:34 +0900

    플레이덱에 뷰 추가방식 일반추가로 변경, 위치,사이즈 조정함수 추가

--- CardGameApp/CardGameApp/views/PlayDeckView.swift
+++ CardGameApp/CardGameApp/views/PlayDeckView.swift
@@ -12,10 +12,22 @@ class PlayDeckView: UIStackView {
 
     init(cardSize: CardSize, x: CGFloat, y: CGFloat){
         super.init(frame: CGRect(origin: CGPoint(x: x, y: y), size: cardSize.cardSize))
-        self.axis = .vertical
     }
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
     }
+    
+    /// 카드를 겹쳐보이게 뷰 추가시 세로위치를 조정해서 추가
+    func addPlayCardview(_ view: UIView) {
+        // 마지막 카드가 있는지 체크
+        if let lastView = self.subviews.last {
+            // 마지막 카드가 있다면 추가되는 카드의 위치는 마지막 카드 높이 + 카드길이/4
+            let spacing = lastView.frame.origin.y + lastView.frame.height / 4
+            view.frame.origin.y += spacing
+        }
+        // 서브뷰로 추가
+        self.addSubview(view)
+    }
 }
+
--- CardGameApp/CardGameApp/views/PlayDeckViewManager.swift
+++ CardGameApp/CardGameApp/views/PlayDeckViewManager.swift
@@ -10,33 +10,42 @@ import UIKit
 
 /// 플레이덱뷰 를 가지는 객체
 class PlayDeckViewManager : UIView{
+    /// 각 라인덱을 배열로 같는다
+    var deckList : [PlayDeckView] = []
+    
     
     /// 카드사이즈와 좌표를 받아서 스택뷰 구성
     private func makeDeckViews(cardSize: CardSize, xPositions: [CGFloat], yPositions: [CGFloat]){
         for count in 0..<cardSize.maxCardCount {
             // 스택뷰 구성
             let stackView = PlayDeckView(cardSize: cardSize, x: xPositions[count], y: yPositions[0])
+            // 일반추가함
             self.addSubview(stackView)
+            deckList.append(stackView)
         }
     }
     
     /// 플레이덱매니저 초기세팅함수
     func setting(cardSize: CardSize, xPositions: [CGFloat], yPositions: [CGFloat]){
+        // 위치, 사이즈 조정
         self.frame.origin.x = 0
         self.frame.origin.y = yPositions[1]
         self.frame.size.width = cardSize.screenWidth
         self.frame.size.height = cardSize.screenHeight - yPositions[1]
-        
+        // 내부 덱뷰를 생성한다
         makeDeckViews(cardSize: cardSize, xPositions: xPositions, yPositions: yPositions)
     }
     
-    /// 뷰를 받아서 추가
+    /// 덱라인, 뷰를 받아서 추가
     func addView(view: UIView, deckLine: Int){
-        self.subviews[deckLine].addSubview(view)
+        // 덱 라인에 맞는 스택뷰 추출
+        let stackView = self.deckList[deckLine]
+        // 맞는 스택뷰에 추가
+        stackView.addPlayCardview(view)
     }
     
     /// 과거카드데이터를 받아서 해당 뷰 리턴
     func getView(pastCardData: PastCardData) -> UIView? {
-        return self.subviews[pastCardData.deckLine].subviews.last
+        return self.deckList[pastCardData.deckLine].subviews.last
     }
 }
