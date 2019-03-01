053a28e1c9745c6de3d9399410a8cf8a05269d1b
DrakeYang <drake.the.programmer@gmail.com>
2019-03-01 15:21:32 +0900

    카드뷰가 직접 카드인포 다루는 부분 수정 : 카드뷰모델

--- CardGameApp/CardGameApp/views/CardView.swift
+++ CardGameApp/CardGameApp/views/CardView.swift
@@ -17,17 +17,18 @@ protocol CardViewInfo {
 
 /// 카드 표현을 담당하는 이미지뷰
 class CardView : UIImageView, CardViewInfo {
-    // 카드 이름
-    var cardInfo : CardInfo = Card(mark: .spade, numbering: .ace, deckType: .deck)
+    // 카드뷰모델
+    var cardViewModel = CardViewModel(cardInfo: Card(mark: .spade, numbering: .ace, deckType: .deck))
     // 카드 앞면 이미지
     private var cardFrontImage = UIImage()
     // 뒷면 이미지는 공통
     private let cardBackImage = #imageLiteral(resourceName: "card-back")
     /// 프레임 받아서 생성
     init(cardInfo: CardInfo, frame: CGRect){
-        self.cardInfo = cardInfo
-        let cardImage = UIImage(named: cardInfo.name()) ?? UIImage()
-        self.cardFrontImage = cardImage
+//        self.cardInfo = cardInfo
+        self.cardViewModel = CardViewModel(cardInfo: cardInfo)
+//        let cardImage = UIImage(named: cardInfo.name()) ?? UIImage()
+//        self.cardFrontImage = cardImage
         super.init(frame: frame)
         // 앞뒷면 상태에 따라 이미지 표시
         self.refreshImage()
@@ -36,9 +37,12 @@ class CardView : UIImageView, CardViewInfo {
     }
     /// 사이즈를 받아서 0,0 에 생성
     init(cardInfo: CardInfo, size: CGSize){
-        self.cardInfo = cardInfo
-        let cardImage = UIImage(named: cardInfo.name()) ?? UIImage()
-        self.cardFrontImage = cardImage
+//        self.cardInfo = cardInfo
+//        let cardImage = UIImage(named: cardInfo.name()) ?? UIImage()
+//        self.cardFrontImage = cardImage
+        
+        self.cardViewModel = CardViewModel(cardInfo: cardInfo)
+        
         super.init(frame: CGRect(origin: CGPoint(x:0, y:0), size: size))
         // 앞뒷면 상태에 따라 이미지 표시
         self.refreshImage()
@@ -53,27 +57,22 @@ class CardView : UIImageView, CardViewInfo {
     
     /// 카드 앞뒷면 상태 체크
     func isFront() -> Bool {
-        return self.cardInfo.name() == self.cardInfo.image()
+        return self.cardViewModel.isFront()
     }
     
     /// 카드뷰를 뒤집을 경우 뒤집은 후의 이미지로 교체한다
     func flip(){
-        self.cardInfo.flip()
+        self.cardViewModel.flip()
         refreshImage()
     }
     
     /// 카드인포 내부에서 카드가 뒤집힐 경우를 위한 이미지 갱신
     func refreshImage(){
-        if isFront() {
-            self.image = self.cardFrontImage
-        } else {
-            
-            self.image = self.cardBackImage
-        }
+        self.image = self.cardViewModel.refreshImage()
     }
     
     /// 프로토콜 준수
     func name() -> String {
-        return self.cardInfo.name()
+        return self.cardViewModel.name()
     }
 }
