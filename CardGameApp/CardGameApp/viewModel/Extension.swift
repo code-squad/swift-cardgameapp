859360e29eaf37d0eed7c973086d1c1ce2acd3a1
DrakeYang <drake.the.programmer@gmail.com>
2019-03-05 14:26:13 +0900

    CGPoint 두 포인트 합계와 자기포인트 추가함수 추가

--- CardGameApp/CardGameApp/viewModel/Extension.swift
+++ CardGameApp/CardGameApp/viewModel/Extension.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2019년 Drake. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+
+extension CGPoint {
+    /// 다른 포인트를 받아서 x,y 좌표를 각각 더한 값을 리턴한다
+    func getPlusedPoint(point: CGPoint) -> CGPoint{
+        let xPoint = self.x + point.x
+        let yPoint = self.y + point.y
+        return CGPoint(x: xPoint, y: yPoint)
+    }
+    
+    /// 다른 포인트를 받아서 x,y 만큼 추가한다
+    mutating func addPosition(point: CGPoint){
+        self.x = point.x
+        self.y = point.y
+    }
+}
