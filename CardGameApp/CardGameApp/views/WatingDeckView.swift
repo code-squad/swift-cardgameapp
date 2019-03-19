89c7ecd7a1169468e3bb743fb85d62e7f9526825
DrakeYang <drake.the.programmer@gmail.com>
2019-03-19 12:19:02 +0900

    웨이팅덱뷰에 카드추가기능 추가

--- CardGameApp/CardGameApp/views/WatingDeckView.swift
+++ CardGameApp/CardGameApp/views/WatingDeckView.swift
@@ -6,4 +6,12 @@
 //  Copyright © 2019년 Drake. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class WatingDeckView : UIView {
+    /// 뷰를 받아서 추가하고 추가된 뷰의 위치를 리턴한다. 웨이팅덱뷰는 그냥 0.0
+    func addView(cardView: CardView) -> CGPoint {
+        self.addSubview(cardView)
+        return self.frame.origin
+    }
+}
