4ccaa00764819f08b9f69a1252d519ad06e83dac
kiljoonho <kiljh@me.com>
2019-07-20 13:25:52 +0900

    카드 덱을 관리하는 뷰 생성

---
+++ CardGameApp/CardGameApp/CardDeckView.swift
@@ -0,0 +1,21 @@
+//
+//  CardDeckView.swift
+//  CardGameApp
+//
+//  Created by joon-ho kil on 7/20/19.
+//  Copyright © 2019 길준호. All rights reserved.
+//
+
+import UIKit
+
+class CardDeckView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
